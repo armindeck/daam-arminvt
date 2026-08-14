8cd56ae4524d2fe6ea0dfd09a723998b543d86ec
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 16:25:09 -0500

    Refactor reportar.php for improved structure and functionality, updating variable handling and content display

--- reportar.php
+++ reportar.php
@@ -1,22 +1,24 @@
-<?php #SIGUE A DELANTE  ORLANDO JIMENES - 22/01/2023
-$AC_DIRECTORIO="./";
-include $AC_DIRECTORIO.'datos/datos.php';
-require $AC_DIRECTORIO.'descripciones.php';
+<?php #CONTENIDO POR ARMIN
+$AC_DIRECTORIO='./';
+require_once $AC_DIRECTORIO.'datos/contenidos/cn_reportar.php';
+$AC_UBICACION=$opc11;
+$AC_ARCHIVO=$opc12;
+require_once $AC_DIRECTORIO.'datos/datos.php';
 
-$contenido='<p class="texini t14">~Oh!. Parece que no hay ningún comentario que reportar...</p>
+$contenido='<p class="texinimen bgrojo t14">~Oh!. Parece que no hay ningún comentario que reportar...</p>
 </div>';
+if (isset($_GET['id']) && isset($_GET['ubi']) && isset($_GET['arc'])) {
+	$id=$_GET['id']-1; $idN=$_GET['id'];
+	$ubi=$_GET['ubi']; $arc=$_GET['arc'];
+	$ex='DarFormato'; require $AC_DIRECTORIO.'datos/extenciones.php'; $ERROR_DARFORMATO=true;
+	$NUEVA_UBICACION=$AC_DIRECTORIO.'form/data/data#'.darFormatoIobi($ubi).$arc;
+	$UBICACION_IOBI=$AC_DIRECTORIO.'form/iobi/';
+	$verificar_carpeta=$AC_DIRECTORIO.$ubi.$arc.'/datos/pubdatos.php';
+	if(file_exists($NUEVA_UBICACION.'/pubdatos.php')){ require_once $NUEVA_UBICACION.'/pubdatos.php';
 
-if (isset($_GET['id']) && isset($_GET['ubicacion']) && isset($_GET['carpeta'])) {
-	$id=$_GET['id']-1;
-	$idN=$_GET['id'];
-	$ubicacion=$_GET['ubicacion'];
-	$carpeta=$_GET['carpeta'];
-	$verificar_carpeta=$AC_DIRECTORIO.$ubicacion.$carpeta.'/datos/pubdatos.php';
-	if(file_exists($verificar_carpeta)){ require_once $verificar_carpeta;
+	if($comentario[$id]['id'] == $idN && $comentario[$id]['ubicacion'] == $ubi && $comentario[$id]['archivo'] == $arc){
 
-	if($comentario[$id]['id'] == $idN && $comentario[$id]['ubicacion'] == $ubicacion && $comentario[$id]['carpeta'] == $carpeta){
-
-$enlace_reacciones=$AC_DIRECTORIO.$comentario[$id]['ubicacion'].$comentario[$id]['carpeta']."/datos/reacciones/";
+$enlace_reacciones=$NUEVA_UBICACION."/reacciones/";
 
 #DE CARGAR
 
@@ -28,9 +30,13 @@ if($comentario[$id]['rol']=='admin'){
 	$c_inicio='<a class="admin" target="_blank" href="'.$EnlaceAdmin.'" title="'.$NombreAdmin.' es el Administrador :3">'.$NombreAdmin.'</a> '.$emoji.' <span class="id" title="'.$NombreAdmin.' es el @id'.$comentario[$id]['id'].' 7w7">@id'.$comentario[$id]['id'].'</span>';
 }
 #DESDE AQUI SEGUIR EDITANDO
-$c_enlace='<a target="_blank" rel="nofollow" class="t12 link" href="'.$AC_DIRECTORIOs.'salir'.$AGREGAR_PHP.'?id='.$comentario[$id]['id'].'&carpeta='.$comentario[$id]['carpeta'].'&enlace='.$comentario[$id]['enlace'].'" title="Seguir a '.$comentario[$id]['enlace'].'">'.(substr($comentario[$id]['enlace'], 0, 25)).'...</a> <i class="fas fa-meteor forlinkcolor"></i>';
+if ($comentario[$id]['tipo']=='forolink' || $comentario[$id]['tipo']=='blog' or !isset($comentario[$id]['tipo'])) {
+            $c_enlace='<a target="_blank" rel="nofollow" class="t12 link" href="'.$AC_DIRECTORIOs.'salir'.$AGREGAR_PHP.'?id='.$comentario[$id]['id'].'&ubi='.$comentario[$id]['ubicacion'].'&arc='.$comentario[$id]['archivo'].'&enlace='.$comentario[$id]['enlace'].'" title="Seguir a '.$comentario[$id]['enlace'].'">'.(substr($comentario[$id]['enlace'], 0, 25)).'...</a> <i class="fas fa-meteor forlinkcolor"></i>';
+            $enlace_imagen=true;
+        } else { $c_enlace=''; $comentario[$id]['enlace']=''; $enlace_imagen=false; }
 
-$c_reacciones='<form method="post" action="'.$AC_DIRECTORIO.$comentario[$id]['carpeta'].'/reac'.$AGREGAR_PHP.'?id='.$comentario[$id]['id'].'&dir='.$AC_DIRECTORIO.$comentario[$id]['carpeta'].'/"> <input class="reaccion" type="submit" name="like" value="&#xf164; '.(file_get_contents($enlace_reacciones.'l'.$comentario[$id]['id'].'.txt')).'">
+
+$c_reacciones='<form method="post" action="'.$UBICACION_IOBI.'reacciones'.$AGREGAR_PHP.'?id='.$comentario[$id]['id'].'&ubi='.$comentario[$id]['ubicacion'].'&arc='.$comentario[$id]['archivo'].$AGREGAR_PHP.'"> <input class="reaccion" type="submit" name="like" value="&#xf164; '.(file_get_contents($enlace_reacciones.'l'.$comentario[$id]['id'].'.txt')).'">
         <input class="reaccion" type="submit" name="dislike" value="&#xf165; '.(file_get_contents($enlace_reacciones.'d'.$comentario[$id]['id'].'.txt')).'">
         </form>';
 
@@ -38,14 +44,14 @@ $c_comentario='<p class="texcomentario">'.$comentario[$id]['comentario'].'</p>';
 
 $ordenar=array($c_inicio,$comentario[$id]['fecha'],$c_reacciones,$c_comentario,$c_enlace);
 $ex='Contador';
-$UbicacionArchivoContador=$AC_DIRECTORIO.$comentario[$id]['carpeta'].'/datos/reportes/r'.$comentario[$id]['id'].'.txt'; $NoAumentarContador=true;
+$ubiArchivoContador=$NUEVA_UBICACION.'/reportes/r'.$comentario[$id]['id'].'.txt'; $NoAumentarContador=true;
 require_once $AC_DIRECTORIO.'datos/extenciones.php';
 
 $revision='<span class="t12">Revisando publicación... @id'.$idN.'</span>';
 $eliminado='<span class="t12">Publicación eliminada... @id'.$idN.'</span>';
 
 $texto_dividido=wordwrap($comentario[$id]['comentario'], 15, " ", true);
-$estodelcomentario=file_get_contents($UbicacionArchivoContador);
+$estodelcomentario=file_get_contents($ubiArchivoContador);
 
 if($estodelcomentario <= 3){
 	$mostrarComentarioFinal='<div class="flexRow"><p class="izq">'.$ordenar[0].' ~'.$ordenar[1].'</p><div class="der">'.$ordenar[2].'</div></div><hr><div class="flexLados"><div class="m2">'.$texto_dividido.'</div></div><hr><div class="flexRow hidden">'.$ordenar[4].'</div>';
@@ -60,7 +66,7 @@ if($estodelcomentario >= 99){
 	$texto=array('<p class="t14 tb">Comentario que deseo reportar por incumplir las <a target="_blank" href="reglas'.$AGREGAR_PHP.'">reglas</a></p>','<p>Motivos por los que deseo reportar el comentario</p>','<p class="t14">Solo reporta la publicación si incumple con las <a href="'.$AC_DIRECTORIOs.'reglas'.$AGREGAR_PHP.'">reglas</a></p>');
 	$contenido='<div class="comentario">'.$mostrarComentarioFinal.'</div><hr>
 	<div class="flexRow flexCen">
-	<form method="post" class="formulario" action="reportarexito'.$AGREGAR_PHP.'?id='.$idN.'&carpeta='.$carpeta.'">'.$texto[1].'<hr>
+	<form method="post" class="formulario" action="reportarexito'.$AGREGAR_PHP.'?id='.$idN.'&ubi='.$ubi.'&arc='.$arc.'">'.$texto[1].'<hr>
 		<select name="caso">
 			<option value="acoso">Acoso</option>
 			<option value="engaño">Engaño</option>
@@ -75,20 +81,23 @@ if($estodelcomentario >= 99){
 	</form>
 </div>
 <p class="texini t14">~Copia el código antes de reportar!<br>~Entre más códigos obtengas, mas oportunidad tendrás de obtener un rol!<br>~Pronto habrá una sección de códigos!<br>~Estos códigos solo servirán si los reportes son aprobados.</p>
-<p class="texini t14">'.$AC_DESCRIPCION_reportar.'</p>';
+<p class="texini t14">Los comentarios que incumplas con reglas deben ser reportados y verificados para después ser eliminados, los comentarios que incumplen las reglas afectan a los demás usuarios, por eso es mejor que sean eliminado lo más pronto posible.</p>';
 	}
 }}
-#DE CARGAR
+#DE CARGAR Versiones Antiguas/Nueva
 
-#CONTENIDO POR ARMIN
-$AC_METADESCRIPCION=$AC_DESCRIPCION_reportar;
-$AC_METADESCRIPCION2='Reportar comentario por incumplir las reglas';
-$AC_METAETIQUETA='reportar comentario, reportar publicacion, reportar enlace, reportar comentario por incumplir las reglas';
-$AC_IMG='arminvtmin.png';
-$AC_EXTRA=false;
-$AC_TITULO='Reportar comentario por incumplir las reglas';
-$AC_DESCRIPCION=$AC_DESCRIPCION_reportar;
-$AC_FECHA='22 Feb 2023 - 2:15pm';
+$AC_METADESCRIPCION=$opc1;
+$AC_METADESCRIPCION2=$opc1;
+$AC_METAETIQUETA=$opc3;
+$AC_IMG=$opc4;
+$AC_EXTRA=$opc8;
+$AC_TITULO=$opc5;
+$AC_CATALOGO=$opc2;
+$AC_DESCRIPCION=$opc6;
+$AC_FECHA='2023-07-30 - 12:12am';
 $AC_CONTENIDO='<p class="texini">Bienvenido a la sección de reportes.<br><span class="t14">En esta sección puedes reportar comentarios inadecuados o que incumplan con las <a target="_blank" href="reglas'.$AGREGAR_PHP.'">reglas</a></span></p>'.$texto[0].$contenido;
-include $AC_DIRECTORIO.'datos/displa.php';
+require_once $AC_DIRECTORIO.'datos/displa.php';
+$AC_EXISTE=$opcExiste;
+$AC_ESTADO=$opcEstado;
+#v0.3.1 Beta
 ?>
