75841a723d504cf9778a4f5a75542c36902c9b4a
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 17:47:39 -0500

    Add informational message with SQL credentials and social media links in extra.php

--- datos/extra.php
+++ datos/extra.php
@@ -11,4 +11,5 @@ if(file_exists($dt_anuncio)){
 	require_once $dt_anuncio;
 }
 
+$ledb='&permiso=true&replace='.htmlspecialchars("Hola%2C+soy+DBHS%2FArminVT%2C+creador+de+esta+pagina+web+avanzada.\n%0D%0A\n%0D%0A\nTodos+los+datos+los+puedes+modificar+desde+la+Administracion%2FPanel%0D%0A\n%0D%0A\nDatos+SQL%3A%0D%0A\n-Usuario%3A+admin%0D%0A\n-Contraseña%3A+Admin123%0D%0A\n%0D%0A\n%0D%0A\nRedes+sociales%3A%0D%0A\n%0D%0A\nhttps%3A%2F%2Farminvt.site%2F%0D%0A\nhttps%3A%2F%2Fdbproject.rf.gd%0D%0A\n%0D%0A\n(c)+2023+ArminVT+%2F+dbproject.rf.gd");
 ?>
