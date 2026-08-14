0316c6c435f61fdca922a95fb6c8f11a294190c1
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 15:31:42 -0500

    Refactor borrador.php to streamline form handling and improve code readability

--- administracion/panel/borrador.php
+++ administracion/panel/borrador.php
@@ -1,46 +1,27 @@
 <?php #BASE CREADA POR ARMIN
 $AC_DIRECTORIO='../../';
+require_once $AC_DIRECTORIO.'datos/datos.php';
 if (!empty($_POST) || !empty($_GET)){
     if (isset($_POST['publicar'])) {
-        $opc1=$_POST['opc1']; #Meta descripción
-        $opc2=$_POST['opc2']; #Catalogo
-        $opc3=$_POST['opc3']; #Meta etiqueta
-        $opc4=$_POST['opc4']; #Imagen
-        $opc5=$_POST['opc5']; #Titulo
-        $opc6=$_POST['opc6']; #Descripción breve
-        $opc7=$_POST['opc7']; #Contenido
-        $opc8=$_POST['opc8']; #Anuncio
-        $opc9=$_POST['opc9']; #Tipo
-        $opc10=$_POST['opc10']; #Directorio
-        $opc11=$_POST['opc11']; #Ubicacion : <Entradas>
-        $opc12=$_POST['opc12']; #Archivo
+        $exP='exPMetodo'; $exP_Metodo='POST'; require 'extencionesPanel.php'; 
     }
     
     if (isset($_GET['borradores']) && isset($_GET['opcBorrador'])) {
         $num=$_GET['opcBorrador'];
-        $borradorE=false;
-        $eliminar=true;
-        if (!file_exists('borradores')) {
-            $ex='IniCrearCarpeta';
-            $acceso=true;
-            require_once 'actualizar_acc.php';
+        #$borradorE=false;
+        #$eliminar=true;
+        
+        $arc="borradores/$num.php";
+        if (file_exists($arc)) {
+            require_once $arc;
+            #$borradorE=true;
         } else {
-            $arc="borradores/$num.php";
-            if (file_exists($arc)) {
-                require_once $arc;
-                $borradorE=true;
-            } else {
-                header("Location: panel.php?ac=creador&ms=err&msm=noexisarchivo");
-            }
+            header("Location: panel.php?ac=creador&ms=err&msm=noexisarchivo");
         }
     }
 
-    if ($opc8=='si' or $opc8==true) {
-        $anuncioS=true;
-    }
-    if ($opc8=='no' or $opc8==false) {
-        $anuncioS=false;
-    }
+    $exP='exPOpc8'; require 'extencionesPanel.php';
+
     if ($opc9!='normal'){
         $AC_CARPETA=$opc10;
         $TIPO=$opc9;
@@ -52,11 +33,28 @@ if (!empty($_POST) || !empty($_GET)){
     $AC_METADESCRIPCION2=$opc1;
     $AC_METAETIQUETA=$opc3;
     $AC_IMG=$opc4.'.png';
-    $AC_EXTRA=$anuncioS;
+    $AC_EXTRA=$opc8V;
     $AC_TITULO=$opc5;
     $AC_CONTENIDO=$opc7;
-    $informacion="Directorio: <b>$opc10</b> | Ubicación: <b>$opc11</b> | Archivo: <b>$opc12</b> | Tipo: <b>$opc9</b> | Anuncio: <b>$opc8</b> | Imagen: <b>$opc4</b> | Catalogo: <b>$opc2</b>";
-    $datos_introducidos="&opc1=$opc1&opc2=$opc2&opc3=$opc3&opc4=$opc4&opc5=$opc5&opc6=$opc6&opc8=$opc8&opc9=$opc9&opc10=$opc10&opc11=$opc11&opc12=$opc12";
+    if (!empty($_POST['ModArchivo'])) {
+        $ArchivoEditable=$_POST['ModArchivo'];
+        $ArchivoOpcEditable=$_POST['opcModArchivo'];
+        $SeModifica=true;
+    } else if (isset($opcEdi) && isset($opcEdi2)) {
+        $ArchivoEditable=$opcEdi;
+        $ArchivoOpcEditable=$opcEdi2;
+        $SeModifica=true;
+    } else { $ArchivoEditable = ''; $ArchivoOpcEditable = ''; $SeModifica=false; }
+
+    if ($SeModifica) {
+        $ModArchivo='&ModArchivo='.$ArchivoEditable;
+        $opcModArchivo='&opcModArchivo='.$ArchivoOpcEditable;
+        $InfoEditable='ArchivoEditable: <b>'.$ArchivoEditable.'</b> | ';
+        $InfoOpcEditable='Modifica: <b>'.$ArchivoOpcEditable.'</b> |';
+    } else { $InfoEditable=''; $InfoOpcEditable=''; $ModArchivo=$ArchivoEditable; $opcModArchivo=$ArchivoOpcEditable; }
+
+    $informacion="Directorio: <b>$opc10</b> | Ubicación: <b>$opc11</b> | $InfoEditable$InfoOpcEditable Archivo: <b>$opc12</b> | Tipo: <b>$opc9</b> | Anuncio: <b>$opc8V</b> | Imagen: <b>$opc4</b> | Catalogo: <b>$opc2</b>";
+    $datos_introducidos="&opc1=$opc1&opc2=$opc2&opc3=$opc3&opc4=$opc4&opc5=$opc5&opc6=$opc6&opc8=$opc8V&opc9=$opc9&opc10=$opc10&opc11=$opc11&opc12=$opc12".$ModArchivo.$opcModArchivo;
     $ubiBorrador='borrador';
 ?>
 <!DOCTYPE html>
@@ -68,33 +66,34 @@ if (!empty($_POST) || !empty($_GET)){
 	<div class="der">
         <div class="flexRow">
         <a class="boton" href="panel.php?ac=creador">Cancelar</a>
-        <?php if (isset($_GET['opcBorrador'])) {  ?>
-            <form action="crear_acc.php?eli=<?php echo $_GET['opcBorrador']; ?>" method="post">
+        <?php if (isset($_GET['opcBorrador']) || isset($SeModifica)): 
+            if (isset($_GET['opcBorrador'])) { $vamosaeli=$_GET['opcBorrador'].'&tipo=borrador'; $DenegarEliBo=true; }
+            if (isset($SeModifica) && !isset($DenegarEliBo)) { $vamosaeli=$ArchivoEditable.'&tipo=publicada'; }
+        ?>
+            <form action="crear_acc.php?eli=<?php echo $vamosaeli; ?>" method="post">
                 <input type="submit" name="eliminar" class="boton2" value="Eliminar">
             </form>
-        <?php } ?>
+        <?php endif; ?>
 		<form action="panel.php?ac=creador&di=true<?php if (isset($_GET['opcBorrador'])) { echo '&opcBorrador='.$_GET['opcBorrador']; } echo htmlspecialchars($datos_introducidos); ?>" method="post">
             <input class="oculto" type="text" name="opc7" value="<?php echo htmlspecialchars($opc7); ?>">
             <input class="boton" type="submit" value="Editar">
         </form>
+        <?php if (!empty($_POST['publicar'])): ?>
         <form action="crear_acc.php?di=true<?php if (isset($_GET['opcBorrador'])) { echo '&opcBorrador='.$_GET['opcBorrador']; } echo htmlspecialchars($datos_introducidos); ?>&ms=exi&msm=datosactualizados" method="post">
             <input class="oculto" type="text" name="opc7" value="<?php echo htmlspecialchars($opc7); ?>">
             <input type="submit" name="guardar" class="boton" value="Guardar">
         </form>
-        <?php if (!isset($publicar)) { ?>
+        <?php endif; ?>
+        <?php if (!empty($_GET['borradores'])): ?>
             <form action="crear_acc.php?<?php if (isset($_GET['opcBorrador'])) { echo 'opcBorrador='.$_GET['opcBorrador']; } ?>" method="post">
     			<input type="submit" name="publicar" class="boton2" value="Publicar">
     		</form>
-        <?php } ?>
+        <?php endif; ?>
         </div>
 	</div>
 </header><hr>
-<ol>
-	<li><?php if (isset($ubiCrearCC)) { echo $ubiCrearCC.' > '; } if (isset($ubiBorrador)) { echo $ubiBorrador; } echo ' || '.$informacion; ?></li>
-</ol>
+<p class="texini"><?php if (isset($ubiCrearCC)) { echo $ubiCrearCC.' > '; } if (isset($ubiBorrador)) { echo $ubiBorrador; } echo ' || '.$informacion; ?></p>
 <?php require_once $AC_DIRECTORIO.'datos/displa.php'; ?>
 </body>
 </html>
-<?php
-    exit;
-} else { $AC_DIREC='../../'; $AC_ENCONTRAR='creador/'; require_once $AC_DIREC.'error.php'; } ?>
+<?php } else { $AC_DIREC='../../'; $AC_ENCONTRAR='creador/'; require_once $AC_DIREC.'error.php'; } ?>
