a618b67cd5b86c8fc024ba339b5085e156c7935f
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 18:18:11 -0500

    Data

---
+++ form/data/forolink/pubdatos.php
@@ -0,0 +1,603 @@
+<?php
+
+$comentario=[
+
+[
+
+'id'=>1,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'admin',
+
+'nombre'=>'Armin',
+
+'enlace'=>'https://youtube.com/@arminvtch?sub_confirmation=1',
+
+'comentario'=>'🎁 Hola, bienvenidos a la nueva versión de ForoLink 🔥<br> 😔Lamento que los comentarios fueran eliminados, pero toco perderlos 😪 ya que el código lo mejore y optimice. <br> ❤️ La nueva versión es mas rápida, mas ordenada y con menos cosas. <br> 🤡El sistema de reportes e imágenes los traeré en otra versión 👋',
+
+'fecha'=>'25 Mar 2023 - 4:07pm'
+
+],
+
+[
+
+'id'=>2,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'Clips somar 98',
+
+'enlace'=>'https://youtube.com/@Somar98',
+
+'comentario'=>'Es un canal de YouTube donde encontrarás clips de tus streamers favoritos',
+
+'fecha'=>'25 Mar 2023 - 5:41pm'
+
+],
+
+[
+
+'id'=>3,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'Akima',
+
+'enlace'=>'https://youtu.be/aBrI-Wb1FyU',
+
+'comentario'=>'&quot;¡Conoce a Akima, la investigadora espacial que ha llegado a la Tierra en busca de nuevos descubrimientos! Acompaña a esta intrépida vtuber en su aventura junto a su fiel robot Ai-Ki y descubre los misterios que esconde nuestro planeta. ¡No te pierdas su video de presentación en nuestro canal de YouTube! #vtuber #akima #aventuraespacial #nuevosdescubrimientos&quot;',
+
+'fecha'=>'26 Mar 2023 - 6:13pm'
+
+],
+
+[
+
+'id'=>4,
+
+'tipo'=>'forolink',
+
+'ubicacion'=>'',
+
+'archivo'=>'forolink',
+
+'rol'=>'usuario',
+
+'nombre'=>'Compañere',
+
+'enlace'=>'https://www.youtube.com/watch?v=4e00sYftzEk',
+
+'comentario'=>'😜Disfruta del nuevo episodio de Fun World Comics: Bajo ataque',
+
+'fecha'=>'27 Mar 2023 - 1:40pm'
+
+],
+
+[
+
+'id'=>5,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'TheFacebook',
+
+'enlace'=>'https://www.youtube.com/watch?v=HEd19gBsHj8',
+
+'comentario'=>'Disfruta la película de como se creo Facebook: Red Social en español Latino',
+
+'fecha'=>'27 Mar 2023 - 1:51pm'
+
+],
+
+[
+
+'id'=>6,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'Pablo',
+
+'enlace'=>'https://www.youtube.com/watch?v=YMwUras_N-Y',
+
+'comentario'=>'cvcsdfasdasdasd',
+
+'fecha'=>'27 Mar 2023 - 2:24pm'
+
+],
+
+[
+
+'id'=>7,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'MR Satan',
+
+'enlace'=>'https://www.youtube.com/watch?v=thBz9b84W2k',
+
+'comentario'=>'Jesse el Guardián, subió nuevo video! ve a verlo :3',
+
+'fecha'=>'29 Mar 2023 - 4:35pm'
+
+],
+
+[
+
+'id'=>8,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'El erizo sexy',
+
+'enlace'=>'https://youtu.be/yAEPk0AChB4',
+
+'comentario'=>'Sobre una canción de metal que hice en mi canal. Soy metalero
+
+🤘😈',
+
+'fecha'=>'31 Mar 2023 - 4:19pm'
+
+],
+
+[
+
+'id'=>9,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'UnSeguidor',
+
+'enlace'=>'https://youtu.be/ESmCkqptTgY',
+
+'comentario'=>'Una animación épica de Zombies creada por Liro in the mist: El Verano y los Muertos | Episodio piloto',
+
+'fecha'=>'01 Apr 2023 - 10:21am'
+
+],
+
+[
+
+'id'=>10,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'basADO',
+
+'enlace'=>'https://youtu.be/4izy26-8h10',
+
+'comentario'=>'VTUBER japonés 🇯🇵 de habla hispano 🇪🇸 muy basado y muy romántico ❤️‍🔥',
+
+'fecha'=>'03 Apr 2023 - 7:48am'
+
+],
+
+[
+
+'id'=>11,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'UnSeguidor',
+
+'enlace'=>'https://www.youtube.com/watch?v=9N_-suV4W_s',
+
+'comentario'=>'Ser vtuber Es facil? mira el nuevo video de Parcival_vt :3',
+
+'fecha'=>'03 Apr 2023 - 4:00pm'
+
+],
+
+[
+
+'id'=>12,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'admin',
+
+'nombre'=>'Armin',
+
+'enlace'=>'https://www.youtube.com/watch?v=WPrnThlqwXI&list=PLKJW6wMWhs7ukPqfmItkNp_a9CFIaKGgX&index=4',
+
+'comentario'=>'Hola! soy Armin y el día de mañana(4 de abril) subiré el primer video dedicado a Game Maker y en el enseñare a hacer un juego completamente desde cero usando Game Maker 8, espero que pasen a ver el video ya que esta epico :3',
+
+'fecha'=>'03 Apr 2023 - 10:53pm'
+
+],
+
+[
+
+'id'=>13,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'admin',
+
+'nombre'=>'Armin',
+
+'enlace'=>'https://www.youtube.com/watch?v=WPrnThlqwXI',
+
+'comentario'=>'Nuevo video! crea tu primer juego rpg con game maker! ve a verlo :3',
+
+'fecha'=>'04 Apr 2023 - 12:48pm'
+
+],
+
+[
+
+'id'=>14,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'admin',
+
+'nombre'=>'Armin',
+
+'enlace'=>'https://youtube.com/@arminvtch',
+
+'comentario'=>'😔 Esta semana no habrá video, ya que me encuentro de viaje :3',
+
+'fecha'=>'10 Apr 2023 - 11:52am'
+
+],
+
+[
+
+'id'=>15,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'UnSeguidor',
+
+'enlace'=>'https://youtu.be/rUVnoIo9GOw',
+
+'comentario'=>'Mira el nuevo video arminvt, la historia de animeflv!',
+
+'fecha'=>'30 Apr 2023 - 10:37pm'
+
+],
+
+[
+
+'id'=>16,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'admin',
+
+'nombre'=>'Armin',
+
+'enlace'=>'https://youtube.com/@arminvtch?sub_confirmation=1',
+
+'comentario'=>'Abecés lo que planeamos no sale como lo pensamos.<br>Cree ForoLinks como un apartado para hablar, pero creo que es mas un sistema de comentarios, estoy orgulloso de esta creación y los avances que hice, una de mis metas es crear un foro o red social que conecte a muchas personas, tengo muchos planos en mi libreta, los cuales los cree el año pasado, poco a poco los fui construyendo, cree apartados épicos.<br>Mis conocimientos actuales no me permiten avanzar, pero se que en algún momento lo lograre.',
+
+'fecha'=>'18 May 2023 - 10:13pm'
+
+],
+
+[
+
+'id'=>17,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'menballfer',
+
+'enlace'=>'https://www.youtube.com/@arminvtch',
+
+'comentario'=>'Hola, soy nuevo en tú canal, me gusta mucho ser programador web, espero poder colaborar contigo en tu sitio web. Saludos 👋🏻',
+
+'fecha'=>'20 May 2023 - 8:38pm'
+
+],
+
+[
+
+'id'=>18,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'admin',
+
+'nombre'=>'Armin',
+
+'enlace'=>'https://youtube.com/@arminvtch',
+
+'comentario'=>'Muchas gracias menballfer, estaría épico que hubieras dejado el link de alguna de tus redes🧃',
+
+'fecha'=>'21 May 2023 - 12:10pm'
+
+],
+
+[
+
+'id'=>19,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'guayareno',
+
+'enlace'=>'https://www.youtube.com/@arminvtch/about',
+
+'comentario'=>'te va quedando cada día bien la pagina (:',
+
+'fecha'=>'23 May 2023 - 12:03am'
+
+],
+
+[
+
+'id'=>20,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'admin',
+
+'nombre'=>'Armin',
+
+'enlace'=>'https://youtube.com/@arminvtch?sub_confirmation=1',
+
+'comentario'=>'Muchas gracias &#039;guayareno&#039; por el comentario, cada ves le implemento mejoras en el diseño y el código para que sea lo mas sencilla y funcional :3',
+
+'fecha'=>'25 May 2023 - 1:08pm'
+
+],
+
+[
+
+'id'=>21,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'menballfer',
+
+'enlace'=>'https://www.facebook.com/mbf.dev',
+
+'comentario'=>'Claro Armin, te dejo mi Facebook para que nos comuniquemos. Sale 👋🏻',
+
+'fecha'=>'28 May 2023 - 5:20pm'
+
+],
+
+[
+
+'id'=>22,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'menballfer',
+
+'enlace'=>'https://arminvt.site/blog/',
+
+'comentario'=>'Muy buena idea del blog!',
+
+'fecha'=>'20 Jun 2023 - 10:49pm'
+
+],
+
+[
+
+'id'=>23,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'Baka',
+
+'enlace'=>'https://BakaOnline.com',
+
+'comentario'=>'Baka Baka Baka',
+
+'fecha'=>'22 Jun 2023 - 10:07am'
+
+],
+
+[
+
+'id'=>24,
+
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+
+'nombre'=>'UnSeguidor',
+
+'enlace'=>'https://www.youtube.com/watch?v=ztCW6cKbvps',
+
+'comentario'=>'Malcriado El Comic: Una Idea Que Termino En Un Baúl <b> Danimacion </b>',
+
+'imagen'=>'https://i.ytimg.com/vi/ztCW6cKbvps/maxresdefault.jpg',
+
+'fecha'=>'13 Jul 2023 - 6:51pm'
+
+],
+
+[
+
+'id'=>25,
+
+'tipo'=>'forolink',
+
+'ubicacion'=>'',
+
+'archivo'=>'forolink',
+
+'rol'=>'admin',
+
+'nombre'=>'Armin',
+
+'enlace'=>'https://arminvt.site/',
+
+'comentario'=>'Disfruta de muchos otros proyectos!',
+
+'imagen'=>'http://arminvt.site/img/arminvtcodigo.png',
+
+'fecha'=>'20 Jul 2023 - 10:17pm'
+
+],
+
+[
+
+'id'=>26,
+
+'tipo'=>'forolink',
+
+'ubicacion'=>'',
+
+'archivo'=>'forolink',
+
+'rol'=>'usuario',
+
+'nombre'=>'now17',
+
+'enlace'=>'https://www.now.com/',
+
+'comentario'=>'this now is now',
+
+'fecha'=>''
+
+],
+[
+'id'=>27,
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'Anonimo',
+'enlace'=>'https://regionpacifica.com/wp-content/uploads/2018/07/mapa_region_pacifica.jpg',
+'comentario'=>'Mapa no. 1 pacifico',
+'imagen'=>'https://regionpacifica.com/wp-content/uploads/2018/07/mapa_region_pacifica.jpg',
+'fecha'=>'2023-07-31 - 2:06pm'
+],
+[
+'id'=>28,
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'pepito',
+'enlace'=>'https://www.youtube.com/watch?v=5mXTuJ7sH1g&list=PLns5S08IJc2xxylXm1tE_xmm6NC4fnfG9&index=2',
+'comentario'=>'de juegos para gamers',
+'fecha'=>'2023-08-02 - 6:20pm'
+],
+[
+'id'=>29,
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'Bin Laden',
+'enlace'=>'https://youtu.be/LCtZIC57pQY',
+'comentario'=>'Allahu akbar',
+'fecha'=>'2023-08-03 - 2:10pm'
+],
+[
+'id'=>30,
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'Bin Laden',
+'enlace'=>'https://youtu.be/LCtZIC57pQY',
+'comentario'=>'Pasen bombas',
+'fecha'=>'2023-08-03 - 2:10pm'
+],
+[
+'id'=>31,
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'Bin Laden',
+'enlace'=>'https://youtu.be/LCtZIC57pQY',
+'comentario'=>'Ala es grande',
+'fecha'=>'2023-08-03 - 2:11pm'
+],
+[
+'id'=>32,
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'yopete',
+'enlace'=>'http://www.ptardo.com',
+'comentario'=>'el bananero pt',
+'imagen'=>'https://worldmusicba.com/wp-content/webp-express/webp-images/uploads/2023/04/contratar-a-el-bananero-07.jpg.webp',
+'fecha'=>'2023-08-04 - 9:15am'
+],
+[
+'id'=>33,
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'juancarlos',
+'enlace'=>'https://juancarlos.com/',
+'comentario'=>'mi sitio web',
+'imagen'=>'https://juancarlos.com/',
+'fecha'=>'2023-08-04 - 9:20am'
+],
+[
+'id'=>34,
+'tipo'=>'forolink',
+'ubicacion'=>'',
+'archivo'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'JesusELH',
+'enlace'=>'https://www.youtube.com/@jesuselh',
+'comentario'=>'suscribete a mi canal papu :VVVVVvvvvvv',
+'fecha'=>'2023-08-11 - 4:40pm'
+]];
