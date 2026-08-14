a5205fd720c28c74e0cccc7c868211c01fa00890
armindeck <dbhsoficial2003@gmail.com>
2026-08-13 21:25:36 -0500

    Datos de los anuncios

---
+++ administracion/panel/sms.php
@@ -0,0 +1,11 @@
+<?php
+$sms='🎁 Disfruta de las ultimas updates de daam por dbproject! ✅';
+$link='https://dbproject.rf.gd/';
+$linkanuncio='https://dbproject.rf.gd/';
+$linkanuncio2='https://dbproject.rf.gd/';
+$linkimga='anuncio.png';
+$linkimga2='min_daamper.png';
+$texMensaje='on';
+$mosAnuncio='on';
+$mosAnuncio2='on';
+?>
