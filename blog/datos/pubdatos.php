398d5f7735d51665004859c6817bbcce21afd854
armindeck <dbhsoficial2003@gmail.com>
2026-08-13 20:45:47 -0500

    Comentarios

---
+++ blog/datos/pubdatos.php
@@ -0,0 +1,57 @@
+<?php
+$comentario=[
+[
+'id'=>1,
+'carpeta'=>'blog',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://arminvt.site/blog/',
+'comentario'=>'Se encuentra disponible la sección de Blog! <br> Disfruta de todas las nuevas actualizaciones y novedades!',
+'fecha'=>'15 Jun 2023 - 4:01pm'
+],
+[
+'id'=>2,
+'carpeta'=>'blog',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://arminvt.site/reglas',
+'comentario'=>'Ya se encuentra disponible la sección de reportes! <br> Ahora puedes reportar los comentarios que incumplan con las reglas!',
+'fecha'=>'15 Jun 2023 - 4:02pm'
+],
+[
+'id'=>3,
+'carpeta'=>'blog',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://arminvt.site/registrar',
+'comentario'=>'Ya se encuentra disponible la sección de registros! <br> Por ahora esta en fase beta, pero le ire haciendo mejoras :3',
+'fecha'=>'21 Jun 2023 - 10:47pm'
+],
+[
+'id'=>4,
+'carpeta'=>'blog',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://arminvt.site/registrar',
+'comentario'=>'Se encuentra disponible la sección de eliminación de cuentas! <br> Si no estas registrado, <b>te invito a registrarte!</b>',
+'fecha'=>'26 Jun 2023 - 4:59pm'
+],
+[
+'id'=>5,
+'carpeta'=>'blog',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://arminvt.site/registrar',
+'comentario'=>'Ya se encuentra disponible los mensajes de emergencias, de esta forma se sabra si hubo errores o registros exitosos.',
+'fecha'=>'26 Jun 2023 - 11:13pm'
+],
+[
+'id'=>6,
+'carpeta'=>'blog',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://arminvt.site/forolink/',
+'comentario'=>'<b> Ahora puedes colocar imagenes! </b> <i> Simplemente coloca el link de alguna imagen! </i>',
+'imagen'=>'https://arminvt.site/img/forolink.png',
+'fecha'=>'13 Jul 2023 - 6:05pm'
+]];
