4b53002f3dfc172b1131880243c0d98325d14d1a
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 16:42:15 -0500

    Refactor extra.php to include scrSecundarios and streamline announcement message handling

--- datos/extra.php
+++ datos/extra.php
@@ -1,22 +1,14 @@
 <?php #CONTENIDO EXTRA
+
 $adminprivado = require_once __DIR__.'/admin_data.php';
 
-require_once $AC_DIRECTORIO.'administracion/panel/extras/extraVariables.php';
+$scrSecundarios=$AC_DIRECTORIO.'administracion/panel/scripts/scrSecundarios.php';
+if(file_exists($scrSecundarios)){ require_once $scrSecundarios; }
+
 #MENSAJE DE INFORMACION Y ANUNCIOS
-require_once $AC_DIRECTORIO.'administracion/panel/sms.php';
-if ($texMensaje == true) { 
-	$texMensaje='<a href="'.$link.'">
-			<marquee direction="left" onmouseout="start();" onmouseover="stop();" scrollamount="10" scrolldelay="145">
-				<p>'.$sms.'</p>
-			</marquee>
-		</a>';
-} else { $texMensaje=''; }
+$dt_anuncio=$AC_DIRECTORIO.'datos/anuncio.php';
+if(file_exists($dt_anuncio)){
+	require_once $dt_anuncio;
+}
 
-if($mosAnuncio==true){
-$anuncio='<hr><a target="_blank" href="'.$linkanuncio.'"><img class="anuncio" src="'.$AC_DIRECTORIO.'img/'.$linkimga.'"></a>';
-} else { $anuncio=''; }
-
-if($mosAnuncio2==true){
-$anuncio2='<a target="_blank" href="'.$linkanuncio2.'"><p class="ctgn">Anuncio</p><img class="anuncio2" src="'.$AC_DIRECTORIO.'img/'.$linkimga2.'"></a>';
-} else { $anuncio2=''; }
 ?>
