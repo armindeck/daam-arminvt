16c35a443e06eef56219622bf008dac166265f0f
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 15:43:15 -0500

    Add new content files for index, projects, and rules, enhancing site information and user engagement

---
+++ datos/contenidos/cn_proyectos-plantasvszombies.php
@@ -0,0 +1,17 @@
+<?php #CONTENIDO POR ARMIN
+$opc1='Cree Plantas Vs Zombies en 5 dias usando Game Maker! ademas se encuentra disponible para Android y PC, links Directos!';
+$opc2='Proyectos';
+$opc3='Plants Vs Zombies FanMade, Cree Plants Vs Zombies en 5 Dias! y asi se ve!';
+$opc4='pvzfan3.png';
+$opc5='Cree Plants Vs Zombies en 5 Dias! y asi se ve!';
+$opc6='Cree Plantas Vs Zombies en 5 dias usando Game Maker! ademas se encuentra disponible para Android y PC, links Directos!';
+$opc7='<p class="texini">Cree Plantas Vs Zombies en 5 dias usando Game Maker! ademas se encuentra disponible para Android y PC, links Directos!</p><p class="texini">Descargar para: <a target="_blank" class="boton" href="https://www.mediafire.com/file/for7d6wrxs6s06l/PvZ_Android.apk/file">Android <i class="fab fa-android"></i></a> - <a target="_blank" class="boton" href="https://www.mediafire.com/file/454befyfx9twfvu/PvZ_Windows.zip/file">Windows <i class="fab fa-windows"></i></a></p><ol><li class="t16  tb">Requisitos</li><li class="t12">Android 6+ / Windows XP+</li><li class="t12">20MB de Espacio</li><li class="t12">Formato: ZIP / APK / EXE</li></ol><div class="flexCon"> <div class="m2">  <div class="imagen"><img class="img1" loading="lazy" src="../img/pvzfan.png"></div> </div> <div class="m2">  <div class="imagen"><img class="img1" loading="lazy" src="../img/pvzfan2.png"></div> </div> <div class="m2">  <div class="imagen"><img class="img1" loading="lazy" src="../img/pvzfan3.png"></div> </div></div>';
+$opc8='si';
+$opc9='normal';
+$opc10='../';
+$opc11='proyectos/';
+$opc12='plantasvszombies';
+$fecha="21 Jul 2023 - 10:02am";
+$opcEstado='publico';
+$opcExiste=true;
+?>
---
+++ datos/contenidos/cn_reglas.php
@@ -0,0 +1,17 @@
+<?php #CONTENIDO POR ARMIN
+$opc1='Estas son las reglas de los forolink';
+$opc2='reglas';
+$opc3='reglas de forolink, las reglas de los forolink, cuales son las reglas de los forolink';
+$opc4='arminvtmin';
+$opc5='Reglas de los forolink';
+$opc6='Las reglas mantienen el equilibrio de los forolink y brindan una mejor comunidad para el entretenimiento y la diversión de los usuarios en la plataforma.';
+$opc7='<p class="texini">¡Hola! ¡Soy <a target="_blank" href="armin">Armin</a> y estas son las reglas de los <a target="_blank" href="forolink/">forolink</a>!</p><p class="texini t14">~Estas reglas con el tiempo serán modificadas para una mejor experiencia, disfruta leyéndolas.</p><ol>    <li class="t12">1. ¡Divertirse!</li>    <li class="t12">2. Ser humilde</li>    <li class="t12">3. Ser amable</li>    <li class="t12">4. Ser respetuoso</li>    <li class="t12">5. Ayudar</li>    <li class="t12">6. No spam</li>    <li class="t12">7. No bullyng</li>    <li class="t12">8. No engañar</li>    <li class="t12">9. No nopor</li>    <li class="t12">10. No pirateria</li></ol><p class="texini t14">~</p>';
+$opc8='si';
+$opc9='normal';
+$opc10='./';
+$opc11='';
+$opc12='reglas2';
+$fecha="20 Jul 2023 - 6:27pm";
+$opcEstado='publico';
+$opcExiste=true;
+?>
