92726cae0fd02013909fdcd86094d58308cec8ca
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 15:37:33 -0500

    Add extraCabeza.php for configuration and user content management

---
+++ administracion/panel/extras/Cabeza/extraCabeza.php
@@ -0,0 +1,31 @@
+<?php #CONTENIDO POR ARMIN
+$Cabeza='on';
+$CabezaCodigos='';
+$CabezaE1='on';
+$CabezaE2='';
+$CabezaE3='';
+$CabezaE4='on';
+$CabezaE1T='';
+$CabezaE2T='';
+$CabezaE3T='';
+$CabezaE4T='';
+$CabezaE1C='';
+$CabezaE2C='';
+$CabezaE3C='';
+$CabezaE4C='';
+$CabezaElementos=4;
+$CabezaScripts='on';
+#ACTUALIZADO: 2023-07-25 - 11:06pm
+?>
+<?php #CONTENIDO POR EL USUARIO
+$CabezaRedes='on';
+$CabezaTituloWeb='on';
+$CabezaTema='on';
+$CabezaRedesFB='on';
+$CabezaRedesYT='on';
+$CabezaRedesTW='on';
+$CabezaRedesTK='on';
+$CabezaRedesPT='on';
+$CabezaRedesKF='on';
+#ACTUALIZADO: 2023-07-25 - 11:06pm
+?>
