dca8e30824f948bc90958f5f8dd46039149b94ab
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 17:41:53 -0500

    Add pubdatos.php: Introduce update log for version 0.3.2 with detailed changes

---
+++ form/data/data#actualizaciones/pubdatos.php
@@ -0,0 +1,43 @@
+<?php
+$comentario=[
+[
+'id'=>1,
+'tipo'=>'blog',
+'ubicacion'=>'',
+'archivo'=>'actualizaciones',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://dbproject.rf.gd/',
+'comentario'=>' <b> Actualizaciones v0.3.2 </b>  <hr> 
+
+ <span class="t14"> Actualizados </span>  <br> 
+
+ <span class="t12"> 
+- <b> Editor » Editor </b>  » editor, Creador, Imagen, Mods, M. Post, A. Acc, P. Acc, Contenido, Displadi + Scripts*,  <br> 
+- <b> Editor » Datos </b>  » Displa, Extenciones <br> 
+- <b> Editor » CSS </b>  » Estilo, Light, Dark <br> 
+
+ </span> 
+
+ <span class="t14"> Eliminados / Retirados </span>  <br> 
+
+ <span class="t12"> 
+- <b> Panel </b>  » Modificador <br> 
+ </span> 
+
+ <span class="t14"> Información </span>  <br> 
+
+ <span class="t12"> 
+- <b> Modificador </b>  » se movio para el apartado de creador <br> 
+- <b> Creador </b>  » se creo el contenedor Herramientas el cual contiene: Borradores, Modificador y Extras, pronto se vienen los scripts <br> 
+- <b> Imagen </b>  » se pueden subir imagenes al directorio de: img <br> 
+- <b> Editor </b>  » se se quito el apartado de modificador, se añadio imagen, y se actualizaron otros. <br> 
+- <b> Configuracion </b>  » se cambio la actualización a v0.3.2 Beta y se añadio enlace admin externo <br> 
+- <b> Displadi </b>  » se actualizaron todos los scripts, se desoculto  &quot; Elementos &quot;  y se pueden poner hasta 4, se añadio  &quot; Contenido Extra &quot;  <br> 
+- <b> A. Acc </b>  » se optimizo el codigo de displadi, se añadio  &quot; subir imagen &quot; , se añadio los directorios  &quot; Creador &quot;  y  &quot; ContenidoExtra &quot;  en IniVerificar, tambien el enlace admin externo <br> 
+- <b> Iobi » procesar </b>  » se oculto el campo de codigo y se amplio el maximo de digitos de los comentarios <br> 
+- <b> Actualizaciones </b>  » se añadio el apartado de actualizaciones <br> 
+- <b> Codigos </b>  » se añadio los codigos de modificaciones <br> 
+ </span> ',
+'fecha'=>'2023-08-23 - 4:07pm'
+]];
