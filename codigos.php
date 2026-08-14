0c8de11ae89dbe69f257851e3e595e2723f462b4
armindeck <dbhsoficial2003@gmail.com>
2026-08-13 20:49:43 -0500

    Codigos para comentarios

---
+++ codigos.php
@@ -0,0 +1,6 @@
+<?php $AC_DIRECTORIO='./'; require_once 'datos/datos.php';
+if(isset($_SESSION['id']) && $_SESSION['rol'] == 5) {
+
+echo htmlspecialchars('<br> = #BR# | <b> = #BA# | </b> = #BC# | <i> = #IA# | </i> = #IC# | <hr> = #HR#');
+
+} else { $AC_DIREC='./'; $AC_ENCONTRAR=''; require_once $AC_DIREC.'error.php'; } ?>
