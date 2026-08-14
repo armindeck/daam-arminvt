20565757295a8cf164bc842b27cf40574f40496e
armindeck <dbhsoficial2003@gmail.com>
2026-08-13 21:25:06 -0500

    Verificar que existen los archivos necesarios

---
+++ administracion/panel/verificar.php
@@ -0,0 +1,22 @@
+<?php if(isset($TIPO)){ if($TIPO='panel'){ ?>
+	<?php
+		if(isset($_GET['dc']) && $_GET['dc'] == 'creados'){
+			echo '<p class="texini bgazul t14">Los directorios: ';
+			if(isset($_GET['ca1'])) echo '<b>'.$_GET['ca1'].'</b>, ';
+			if(isset($_GET['ca2'])) echo '<b>'.$_GET['ca2'].'</b>, ';
+			if(isset($_GET['ca3'])) echo '<b>'.$_GET['ca3'].'</b>';
+			echo ' fueron creadas.</p>';
+		} else if(isset($_GET['dc']) && $_GET['dc'] == 'bien'){
+			echo '<p class="texini bgverde">Todos los directorios se encuentran bien.</p>';
+		}
+	?>
+<p class="texini">Verificar si todos los directorios se encuentran.</p>
+    <div class="flexCon">
+        <form class="formulario" action="actualizar.php" method="post">
+            <p class="t14">Se verificara si los directorios existen, de lo contrario se crearan.</p><hr>
+            <input type="submit" name="IniVerificar" value="Verificar &#xf002">
+        </form>
+    </div>
+</div>
+
+<?php } } else { $AC_DIREC='../../'; $AC_ENCONTRAR=''; include $AC_DIREC.'error.php'; } ?>
