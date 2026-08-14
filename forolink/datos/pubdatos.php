89625af2d0139eeffb4603f24d5ce5bba08fbf09
armindeck <dbhsoficial2003@gmail.com>
2026-08-13 21:01:50 -0500

    Comentarios del forolink

---
+++ forolink/datos/pubdatos.php
@@ -0,0 +1,220 @@
+<?php
+$comentario=[
+[
+'id'=>1,
+'carpeta'=>'forolink',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://youtube.com/@arminvtch?sub_confirmation=1',
+'comentario'=>'🎁 Hola, bienvenidos a la nueva versión de ForoLink 🔥<br> 😔Lamento que los comentarios fueran eliminados, pero toco perderlos 😪 ya que el código lo mejore y optimice. <br> ❤️ La nueva versión es mas rápida, mas ordenada y con menos cosas. <br> 🤡El sistema de reportes e imágenes los traeré en otra versión 👋',
+'fecha'=>'25 Mar 2023 - 4:07pm'
+],
+[
+'id'=>2,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'Clips somar 98',
+'enlace'=>'https://youtube.com/@Somar98',
+'comentario'=>'Es un canal de YouTube donde encontrarás clips de tus streamers favoritos',
+'fecha'=>'25 Mar 2023 - 5:41pm'
+],
+[
+'id'=>3,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'Akima',
+'enlace'=>'https://youtu.be/aBrI-Wb1FyU',
+'comentario'=>'&quot;¡Conoce a Akima, la investigadora espacial que ha llegado a la Tierra en busca de nuevos descubrimientos! Acompaña a esta intrépida vtuber en su aventura junto a su fiel robot Ai-Ki y descubre los misterios que esconde nuestro planeta. ¡No te pierdas su video de presentación en nuestro canal de YouTube! #vtuber #akima #aventuraespacial #nuevosdescubrimientos&quot;',
+'fecha'=>'26 Mar 2023 - 6:13pm'
+],
+[
+'id'=>4,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'Compañere',
+'enlace'=>'https://www.youtube.com/watch?v=4e00sYftzEk',
+'comentario'=>'😜Disfruta del nuevo episodio de Fun World Comics: Bajo ataque',
+'fecha'=>'27 Mar 2023 - 1:40pm'
+],
+[
+'id'=>5,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'TheFacebook',
+'enlace'=>'https://www.youtube.com/watch?v=HEd19gBsHj8',
+'comentario'=>'Disfruta la película de como se creo Facebook: Red Social en español Latino',
+'fecha'=>'27 Mar 2023 - 1:51pm'
+],
+[
+'id'=>6,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'Pablo',
+'enlace'=>'https://www.youtube.com/watch?v=YMwUras_N-Y',
+'comentario'=>'cvcsdfasdasdasd',
+'fecha'=>'27 Mar 2023 - 2:24pm'
+],
+[
+'id'=>7,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'MR Satan',
+'enlace'=>'https://www.youtube.com/watch?v=thBz9b84W2k',
+'comentario'=>'Jesse el Guardián, subió nuevo video! ve a verlo :3',
+'fecha'=>'29 Mar 2023 - 4:35pm'
+],
+[
+'id'=>8,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'El erizo sexy',
+'enlace'=>'https://youtu.be/yAEPk0AChB4',
+'comentario'=>'Sobre una canción de metal que hice en mi canal. Soy metalero
+🤘😈',
+'fecha'=>'31 Mar 2023 - 4:19pm'
+],
+[
+'id'=>9,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'UnSeguidor',
+'enlace'=>'https://youtu.be/ESmCkqptTgY',
+'comentario'=>'Una animación épica de Zombies creada por Liro in the mist: El Verano y los Muertos | Episodio piloto',
+'fecha'=>'01 Apr 2023 - 10:21am'
+],
+[
+'id'=>10,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'basADO',
+'enlace'=>'https://youtu.be/4izy26-8h10',
+'comentario'=>'VTUBER japonés 🇯🇵 de habla hispano 🇪🇸 muy basado y muy romántico ❤️‍🔥',
+'fecha'=>'03 Apr 2023 - 7:48am'
+],
+[
+'id'=>11,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'UnSeguidor',
+'enlace'=>'https://www.youtube.com/watch?v=9N_-suV4W_s',
+'comentario'=>'Ser vtuber Es facil? mira el nuevo video de Parcival_vt :3',
+'fecha'=>'03 Apr 2023 - 4:00pm'
+],
+[
+'id'=>12,
+'carpeta'=>'forolink',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://www.youtube.com/watch?v=WPrnThlqwXI&list=PLKJW6wMWhs7ukPqfmItkNp_a9CFIaKGgX&index=4',
+'comentario'=>'Hola! soy Armin y el día de mañana(4 de abril) subiré el primer video dedicado a Game Maker y en el enseñare a hacer un juego completamente desde cero usando Game Maker 8, espero que pasen a ver el video ya que esta epico :3',
+'fecha'=>'03 Apr 2023 - 10:53pm'
+],
+[
+'id'=>13,
+'carpeta'=>'forolink',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://www.youtube.com/watch?v=WPrnThlqwXI',
+'comentario'=>'Nuevo video! crea tu primer juego rpg con game maker! ve a verlo :3',
+'fecha'=>'04 Apr 2023 - 12:48pm'
+],
+[
+'id'=>14,
+'carpeta'=>'forolink',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://youtube.com/@arminvtch',
+'comentario'=>'😔 Esta semana no habrá video, ya que me encuentro de viaje :3',
+'fecha'=>'10 Apr 2023 - 11:52am'
+],
+[
+'id'=>15,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'UnSeguidor',
+'enlace'=>'https://youtu.be/rUVnoIo9GOw',
+'comentario'=>'Mira el nuevo video arminvt, la historia de animeflv!',
+'fecha'=>'30 Apr 2023 - 10:37pm'
+],
+[
+'id'=>16,
+'carpeta'=>'forolink',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://youtube.com/@arminvtch?sub_confirmation=1',
+'comentario'=>'Abecés lo que planeamos no sale como lo pensamos.<br>Cree ForoLinks como un apartado para hablar, pero creo que es mas un sistema de comentarios, estoy orgulloso de esta creación y los avances que hice, una de mis metas es crear un foro o red social que conecte a muchas personas, tengo muchos planos en mi libreta, los cuales los cree el año pasado, poco a poco los fui construyendo, cree apartados épicos.<br>Mis conocimientos actuales no me permiten avanzar, pero se que en algún momento lo lograre.',
+'fecha'=>'18 May 2023 - 10:13pm'
+],
+[
+'id'=>17,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'menballfer',
+'enlace'=>'https://www.youtube.com/@arminvtch',
+'comentario'=>'Hola, soy nuevo en tú canal, me gusta mucho ser programador web, espero poder colaborar contigo en tu sitio web. Saludos 👋🏻',
+'fecha'=>'20 May 2023 - 8:38pm'
+],
+[
+'id'=>18,
+'carpeta'=>'forolink',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://youtube.com/@arminvtch',
+'comentario'=>'Muchas gracias menballfer, estaría épico que hubieras dejado el link de alguna de tus redes🧃',
+'fecha'=>'21 May 2023 - 12:10pm'
+],
+[
+'id'=>19,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'guayareno',
+'enlace'=>'https://www.youtube.com/@arminvtch/about',
+'comentario'=>'te va quedando cada día bien la pagina (:',
+'fecha'=>'23 May 2023 - 12:03am'
+],
+[
+'id'=>20,
+'carpeta'=>'forolink',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://youtube.com/@arminvtch?sub_confirmation=1',
+'comentario'=>'Muchas gracias &#039;guayareno&#039; por el comentario, cada ves le implemento mejoras en el diseño y el código para que sea lo mas sencilla y funcional :3',
+'fecha'=>'25 May 2023 - 1:08pm'
+],
+[
+'id'=>21,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'menballfer',
+'enlace'=>'https://www.facebook.com/mbf.dev',
+'comentario'=>'Claro Armin, te dejo mi Facebook para que nos comuniquemos. Sale 👋🏻',
+'fecha'=>'28 May 2023 - 5:20pm'
+],
+[
+'id'=>22,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'menballfer',
+'enlace'=>'https://arminvt.site/blog/',
+'comentario'=>'Muy buena idea del blog!',
+'fecha'=>'20 Jun 2023 - 10:49pm'
+],
+[
+'id'=>23,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'Baka',
+'enlace'=>'https://BakaOnline.com',
+'comentario'=>'Baka Baka Baka',
+'fecha'=>'22 Jun 2023 - 10:07am'
+],
+[
+'id'=>24,
+'carpeta'=>'forolink',
+'rol'=>'usuario',
+'nombre'=>'UnSeguidor',
+'enlace'=>'https://www.youtube.com/watch?v=ztCW6cKbvps',
+'comentario'=>'Malcriado El Comic: Una Idea Que Termino En Un Baúl <b> Danimacion </b>',
+'imagen'=>'https://i.ytimg.com/vi/ztCW6cKbvps/maxresdefault.jpg',
+'fecha'=>'13 Jul 2023 - 6:51pm'
+]];
