984421489f96fafdde768dfa4c1ee5f664d4d002
armindeck <dbhsoficial2003@gmail.com>
2026-08-13 20:53:52 -0500

    Procesar la eliminación del perfil

---
+++ perfil_procesar_eliminar.php
@@ -0,0 +1,31 @@
+<?php #CONTENIDO POR ARMIN
+$AC_DIRECTORIO='./';
+require_once $AC_DIRECTORIO.'datos/datos.php';
+if($_SESSION['id']){
+
+require_once $AC_DIRECTORIO.'datos/permisos_usuarios.php';
+
+	if(!empty($_POST['IniEliminar']) && !empty($_POST['motivos']) && !empty($_POST['aceptar']) && !empty($_POST['contrasena'])){
+	    require $AC_DIRECTORIO.'datos/extenciones/extencionDarFormato.php';
+
+	    $motivos=darFormatoNoSimbolos(trim($_POST['motivos']));
+	    $aceptar=darFormatoNoSimbolos(trim($_POST['aceptar']));
+	    $contrasena=md5(darFormato(trim($_POST['contrasena'])));
+	    $id=$_SESSION['id'];
+
+	    if($aceptar=='si' && $contrasena == $_SESSION['contrasena']){
+			$verificar="SELECT * FROM usuarios WHERE id = '$id' or contrasena = '$contrasena'";
+			$resultado=mysqli_query($conexion,$verificar);
+			$row=mysqli_fetch_assoc($resultado);
+
+			if($row['contrasena']){
+				$eliminar="DELETE FROM usuarios WHERE id='$id'";
+
+				$resultado=mysqli_query($conexion,$eliminar);
+				session_destroy();
+				header("Location: iniciar?ms=err&msm=cuentaeliminada");
+			} else { header("Location: perfil_eliminar?ms=err&msm=contrainco"); }
+		} else { header("Location: perfil_eliminar?ms=err&msm=datosnocum"); }
+	}else { header("Location: perfil_eliminar?ms=err&msm=noenvdatos"); }
+} else { header("Location: iniciar?ms=err&msm=noexid"); }
+?>
