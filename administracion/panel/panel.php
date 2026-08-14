0a71327c21575ed9a5f9613c07a8a15b3d0b4033
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 16:36:08 -0500

    Refactor panel.php to replace include with require for data file and fix conditional check for user role validation

--- administracion/panel/panel.php
+++ administracion/panel/panel.php
@@ -1,10 +1,10 @@
 <?php #CONTENIDO POR ARMIN
 $AC_DIRECTORIO='../../';
-include $AC_DIRECTORIO.'datos/datos.php';
+require $AC_DIRECTORIO.'datos/datos.php';
 
 if(isset($_SESSION['id']) && $_SESSION['rol'] == 5){ $acceso=true; require_once 'panel_acc.php'; }
 
-else if(!empty($_SESSION['usuario']) && !empty($_SESSION['codigo']) && !empty($_SESSION['code'])){
+else if(!empty($_SESSION['usuario']) && !empty($_SESSION['codigo']) && !empty($_SESSION['code'] && $_SESSION['rol'])){
     $var=$_SESSION['usuario']; $var2=$_SESSION['codigo']; $var3=$_SESSION['code'];
 
     if($var===$adminprivado['usuario'] && $var2===$adminprivado['codigo'] && $var3===$adminprivado['code']){
@@ -12,5 +12,8 @@ else if(!empty($_SESSION['usuario']) && !empty($_SESSION['codigo']) && !empty($_
         $acceso=true; require_once 'panel_acc.php';
 
     } else { $lugar='../../?ms=err&msm=usuoconocod'; header("Location: {$lugar}"); }
-} else { $AC_DIREC='../../'; $AC_ENCONTRAR=''; include $AC_DIREC.'error.php'; }
+} else {
+    if(isset($AC_DIRECTORIO)){ $AC_DIRECTORIO=$AC_DIRECTORIO; } else { $AC_DIRECTORIO='../../'; }
+        $vamos=$AC_DIRECTORIO."error.php?ms=err&msm=accdenegado"; header("Location: {$vamos}");
+    }
 ?>
