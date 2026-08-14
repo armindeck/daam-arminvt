b20e24925c3beb65f2bdc54112bb9c4616ee5bec
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 17:43:57 -0500

    Add new entries to pubdatos.php: Include updates and corrections for forolink comments and blog features

--- form/data/data#blog/pubdatos.php
+++ form/data/data#blog/pubdatos.php
@@ -55,15 +55,98 @@ $comentario=[
 'comentario'=>'Ya se encuentra disponible los mensajes de emergencias, de esta forma se sabra si hubo errores o registros exitosos.',
 'fecha'=>'26 Jun 2023 - 11:13pm'
 ],
+
 [
-'id'=>6,
+
+'id'=>7,
+
+'tipo'=>'forolink',
+
+'ubicacion'=>'',
+
+'archivo'=>'blog',
+
+'rol'=>'admin',
+
+'nombre'=>'Armin',
+
+'enlace'=>'https://arminvt.site/forolink/',
+
+'comentario'=>'<b>Errores corregidos:</b> <br> <i> -No se muestran los link en forolink comentarios <br> -No se publica en el blog <br> -No cargan los blogs en las entradas nuevas <br> -No se muestra la fecha de publicación <br> -No se puede reaccionar <br> -Fue movido el curso html básico a proyectos </i>',
+
+'fecha'=>'21 Jul 2023 - 1:56pm'
+
+],
+[
+'id'=>8,
 'tipo'=>'blog',
 'ubicacion'=>'',
 'archivo'=>'blog',
 'rol'=>'admin',
 'nombre'=>'Armin',
 'enlace'=>'https://arminvt.site/forolink/',
-'comentario'=>'<b> Ahora puedes colocar imagenes! </b> <i> Simplemente coloca el link de alguna imagen! </i>',
-'imagen'=>'https://arminvt.site/img/forolink.png',
-'fecha'=>'13 Jul 2023 - 6:05pm'
+'comentario'=>'sda56d4as65d4as4das5d4a54sd5',
+'fecha'=>'2023-08-23 - 2:22pm'
+],
+[
+'id'=>9,
+'tipo'=>'blog',
+'ubicacion'=>'',
+'archivo'=>'blog',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://arminvt.site/forolink/',
+'comentario'=>' <b> Errores corregidos: </b>  <br> 
+ <span class="t12"> -No se muestran los link en forolink comentarios  <br>  -No se publica en el blog  <br>  -No cargan los blogs en las entradas nuevas  <br>  -No se muestra la fecha de publicación  <br>  -No se puede reaccionar  <br>  -Fue movido el curso html básico a proyectos  </span> ',
+'fecha'=>'2023-08-23 - 2:57pm'
+],
+[
+'id'=>10,
+'tipo'=>'blog',
+'ubicacion'=>'',
+'archivo'=>'blog',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://jhon-wick.com/nuevasdsa',
+'comentario'=>'dsdsadasdsadsadsadasddasda',
+'fecha'=>'2023-08-23 - 4:01pm'
+],
+[
+'id'=>11,
+'tipo'=>'blog',
+'ubicacion'=>'',
+'archivo'=>'blog',
+'rol'=>'admin',
+'nombre'=>'Armin',
+'enlace'=>'https://jhon-wick.com/nueva',
+'comentario'=>' <b> Actualizaciones v0.3.2 </b>  <hr> 
+
+ <span class="t14"> Actualizados </span>  <br> 
+
+ <span class="t12"> 
+- <b> Editor » Editor </b>  » editor, Creador, Imagen, Mods, M. Post, A. Acc, P. Acc, Contenido, Displadi + Scripts*,  <br> 
+- <b> Editor » Datos </b>  » Displa, Extenciones <br> 
+- <b> Editor » CSS </b>  » Estilo, Light, Dark <br> 
+
+ </span> 
+
+ <span class="t14"> Eliminados / Retirados </span>  <br> 
+
+ <span class="t12"> 
+- <b> Panel </b>  » Modificador <br> 
+ </span> 
+
+ <span class="t14"> Información </span>  <br> 
+
+ <span class="t12"> 
+- <b> Modificador </b>  » se movio para el apartado de creador <br> 
+- <b> Creador </b>  » se creo el contenedor Herramientas el cual contiene: Borradores, Modificador y Extras, pronto se vienen los scripts <br> 
+- <b> Imagen </b>  » se pueden subir imagenes al directorio de: img <br> 
+- <b> Editor </b>  » se se quito el apartado de modificador, se añadio imagen, y se actualizaron otros. <br> 
+- <b> Configuracion </b>  » se cambio la actualización a v0.3.2 Beta <br> 
+- <b> Displadi </b>  » se actualizaron todos los scripts, se desoculto  &quot; Elementos &quot;  y se pueden poner hasta 4, se añadio  &quot; Contenido Extra &quot;  <br> 
+- <b> A. Acc </b>  » se optimizo el codigo de displadi, se añadio  &quot; subir imagen &quot; , se añadio los directorios  &quot; Creador &quot;  y  &quot; ContenidoExtra &quot;  en IniVerificar <br> 
+- <b> Iobi » procesar </b>  » se oculto el campo de codigo <br> 
+ </span> ',
+'fecha'=>'2023-08-23 - 4:03pm'
 ]];
