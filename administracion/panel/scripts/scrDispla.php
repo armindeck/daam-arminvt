9a49c88f3a66a560cbe5a6dcf9e3abb95002c891
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 18:42:12 -0500

    Refactor scrDispla.php to improve code structure and update links for better navigation

--- administracion/panel/scripts/scrDispla.php
+++ administracion/panel/scripts/scrDispla.php
@@ -1,42 +1,42 @@
-<?php #scrDispla
-#0.Cabeza, 1. Menu, 2. Contenido, 3. Menu Lateral, 4. Pie
-#0.Mostrar, 1. Cantidad de Elementos, 2. C. Scripts, 3. Elementos
-$displadi[0]=['on',2,[
-		['on','',''],
-		['on','',''],
-		['','',''],
-		['','','']
-	]
-];
-$displadi[1]=['on',1,[
-		['on','',''],
-		['','',''],
-		['','',''],
-		['','','']
-	]
-];
-$displadi[2]=['',1,[
-		['','',''],
-		['','',''],
-		['','',''],
-		['','','']
-	]
-];
-$displadi[3]=['on',4,[
-		['on','Administrador','<a class="boton" target="_blank" href="https://dbproject.rf.gd/">Armin</a>'],
-		['on','Actualizaciones <a target="_blank" href="https://dbproject.rf.gd/blog"> <i class="fas fa-blog iredes"></i></a>',''],
-		['on','Random',''],
-		['on','Extras','']
-	]
-];
-$displadi[4]=['on',4,[
-		['on','<hr><b>Derechos</b>',''],
-		['on','<hr><b>Información</b>','<p class="t14">Disfruta de mas beneficios creando una <a target="_blank" href="https://dbproject.rf.gd/auth/registrar">cuenta</a><br>También puedes elegir los temas que quieras.</p>'],
-		['on','<hr><b>Extras</b>','<p class="t14">Se añadieron nuevas funciones y optimización<br>Recuerda seguirme en todas mis redes sociales</p>'],
-		['on','<hr><b>Acerca de</b>','<a target="_blank" href="https://dbproject.rf.gd/"><i class="fas fa-crown"></i></a> <a target="_blank" href="https://armionegai.rf.gd/reglas"><i class="fas fa-book"></i></a> <a target="_blank" href="https://armionegai.rf.gd/acerca"><i class="fas fa-tractor"></i></a>']
-	]
-];
-$carScripts=true;
-$mosScripts=['','','','','']; #MOSTRAR SCRIPTS EN DISPLADI
-#Modificado: 2024-09-29 - 11:30am ~ v0.3.3 Beta
+<?php #scrDispla
+#0.Cabeza, 1. Menu, 2. Contenido, 3. Menu Lateral, 4. Pie
+#0.Mostrar, 1. Cantidad de Elementos, 2. C. Scripts, 3. Elementos
+$displadi[0]=['on',2,[
+		['on','',''],
+		['on','',''],
+		['','',''],
+		['','','']
+	]
+];
+$displadi[1]=['on',1,[
+		['on','',''],
+		['','',''],
+		['','',''],
+		['','','']
+	]
+];
+$displadi[2]=['',1,[
+		['','',''],
+		['','',''],
+		['','',''],
+		['','','']
+	]
+];
+$displadi[3]=['on',4,[
+		['on','Administrador','<a class="boton" target="_blank" href="https://dbproject.rf.gd/">Armin</a>'],
+		['on','Actualizaciones <a target="_blank" href="https://arminvt.site/actualizaciones"> <i class="fas fa-blog iredes"></i></a>',''],
+		['on','Random',''],
+		['on','Extras','']
+	]
+];
+$displadi[4]=['on',4,[
+		['on','<hr><b>Derechos</b>',''],
+		['on','<hr><b>Información</b>','<p class="t14">Disfruta de mas beneficios creando una <a target="_blank" href="https://arminvt.site/registrar">cuenta</a><br>También puedes elegir los temas que quieras.</p>'],
+		['on','<hr><b>Extras</b>','<p class="t14">Se añadieron nuevas funciones y optimización<br>Recuerda seguirme en todas mis redes sociales</p>'],
+		['on','<hr><b>Acerca de</b>','<a target="_blank" href="https://dbproject.rf.gd/"><i class="fas fa-crown"></i></a> <a target="_blank" href="https://arminvt.site/reglas"><i class="fas fa-book"></i></a> <a target="_blank" href="https://arminvt.site/acerca"><i class="fas fa-tractor"></i></a>']
+	]
+];
+$carScripts=true;
+$mosScripts=['on','on','on','on','on']; #MOSTRAR SCRIPTS EN DISPLADI
+#Modificado: 2024-01-04 - 12:12am ~ v0.3.3 Beta
 ?>
