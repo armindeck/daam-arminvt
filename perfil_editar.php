6faba2f21f788e9d3063086fceb5e7448b2f877d
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 17:16:37 -0500

    Add theme selection dropdown to perfil_editar.php

--- perfil_editar.php
+++ perfil_editar.php
@@ -30,6 +30,15 @@ $AC_CONTENIDO='<p class="texini">Sección de edición de cuenta de '.$_SESSION['
     <label>Nombre</label> <input type="text" name="nombre" placeholder="" value="'.$_SESSION['nombre'].'" required><br>
     <label>Usuario</label> <input type="text" name="usuario" placeholder="" value="'.$_SESSION['usuario'].'" required><br>
     <label>Email</label> <input type="text" name="email" placeholder="" value="'.$_SESSION['email'].'" required><br>
+    <label>Tema</label>
+    <select name="tema">
+        <option value="temaDefault">Default</option>
+        <option value="temaLight">Light</option>
+        <option value="temaLight2">Light2</option>
+        <option value="temaDark">Dark</option>
+        <option value="temaDark2">Dark2</option>
+        <option value="temaBlue">Dark Blue</option>
+    </select><br>
     <label>Red Social</label> <input type="text" name="redsocial" placeholder="" value="'.$redSocial.'" required><br>
     <label>Contraseña</label> <input type="password" name="contrasena" placeholder="Contraseña" required><hr>
     <input type="submit" name="IniActualizar" value="Actualizar">
