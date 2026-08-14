b5c6665daca8306cbeb207b8010e33a226230e20
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 15:37:05 -0500

    Enhance directory verification output by adding additional directory checks and update version display in verificar.php

--- administracion/panel/verificar.php
+++ administracion/panel/verificar.php
@@ -5,6 +5,10 @@
 			if(isset($_GET['ca1'])) echo '<b>'.$_GET['ca1'].'</b>, ';
 			if(isset($_GET['ca2'])) echo '<b>'.$_GET['ca2'].'</b>, ';
 			if(isset($_GET['ca3'])) echo '<b>'.$_GET['ca3'].'</b>';
+			if(isset($_GET['ca4'])) echo '<b>'.$_GET['ca4'].'</b>';
+			if(isset($_GET['ca5'])) echo '<b>'.$_GET['ca5'].'</b>';
+			if(isset($_GET['ca6'])) echo '<b>'.$_GET['ca6'].'</b>';
+			if(isset($_GET['ca7'])) echo '<b>'.$_GET['ca7'].'</b>';
 			echo ' fueron creadas.</p>';
 		} else if(isset($_GET['dc']) && $_GET['dc'] == 'bien'){
 			echo '<p class="texini bgverde">Todos los directorios se encuentran bien.</p>';
@@ -15,6 +19,7 @@
         <form class="formulario" action="actualizar.php" method="post">
             <p class="t14">Se verificara si los directorios existen, de lo contrario se crearan.</p><hr>
             <input type="submit" name="IniVerificar" value="Verificar &#xf002">
+            <span class="t14">v0.3 Beta</span>
         </form>
     </div>
 </div>
