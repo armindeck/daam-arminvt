ce2f59256cdbfd2887121d713cdcb0eed4dc0298
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 16:25:39 -0500

    Refactor reportarexito.php for improved error handling and user feedback, updating variable assignments and content display

--- reportarexito.php
+++ reportarexito.php
@@ -1,67 +1,68 @@
 <?php #CONTENIDO POR ARMIN
 $AC_DIRECTORIO='./';
-include $AC_DIRECTORIO.'datos/datos.php';
-require $AC_DIRECTORIO.'descripciones.php';
-$AC_METADESCRIPCION='Reporte de comentario por infringir las reglas de forolink';
-$AC_METADESCRIPCION2='El reporte fue enviado exitosamente y será revisado lo mas pronto posible.';
-$AC_METAETIQUETA='comentario reportado, reporte de comentario';
-$AC_IMG='arminvtmin.png';
-$AC_EXTRA=true;
-$AC_DESCRIPCION=$AC_DESCRIPCION_reportarexito;
-$AC_FECHA='22 Feb 2023 - 10:07pm';
-$mostrar='<p class="texini">Oh!. Parece que hubo un error al reportar la publicación...</p>
+require_once $AC_DIRECTORIO.'datos/contenidos/cn_reportarexito.php';
+$AC_UBICACION=$opc11;
+$AC_ARCHIVO=$opc12;
+require_once $AC_DIRECTORIO.'datos/datos.php';
+$AC_METADESCRIPCION=$opc1;
+$AC_METADESCRIPCION2=$opc1;
+$AC_METAETIQUETA=$opc3;
+$AC_IMG=$opc4;
+$AC_EXTRA=$opc8;
+$mostrar='<p class="texinimen bgrojo">Oh!. Parece que hubo un error al reportar la publicación...</p>
 <p class="texini t12">Te recomiendo que vuelvas a reportar la publicación desde forolink...</p>
 <p class="texini t12">Los datos ingresados son incorrectos o no cumplieron con los solicitados</p>';
-$AC_TITULO='Oh!. Parece que hubo un error al reportar la publicación...';
-
-if (isset($_GET['id']) && isset($_GET['carpeta'])) {
-	$id=$_GET['id']; $carpeta=$_GET['carpeta'];
-	#PRUEBAS
-/*	$UbicacionArchivoContador=$AC_DIRECTORIO.$carpeta.'/datos/reportes/r'.$id.'.txt';
-	$l=file_get_contents($UbicacionArchivoContador);
-	$l2=$l+1;
-	file_put_contents($UbicacionArchivoContador,$l2);
-	echo $l;
-*/	#PRUEBAS
+$AC_TITULO=$opc5;
+$AC_CATALOGO=$opc2;
+$AC_DESCRIPCION=$opc6;
+$AC_FECHA='2023-07-30 - 12:19am';
+if (isset($_GET['id']) && isset($_GET['ubi']) && isset($_GET['arc'])) {
+	$id=$_GET['id']; $ubi=$_GET['ubi']; $arc=$_GET['arc'];
 	if (!empty($_POST) && !empty($_POST['caso']) && !empty($_POST['motivos'])){
-		require_once $AC_DIRECTORIO.'datos/extenciones/extencionDarFormato.php';
+		$ex='DarFormato'; require $AC_DIRECTORIO.'datos/extenciones.php'; $ERROR_DARFORMATO=true;
 		$caso=darFormato(trim($_POST['caso']));
 		$motivos = darFormato(trim($_POST['motivos']));
-		$verificar_carpeta=$AC_DIRECTORIO.$carpeta.'/datos/pubdatos.php';
+		$NUEVA_UBICACION=$AC_DIRECTORIO.'form/data/data#'.darFormatoIobi($ubi).$arc;
+		$UBICACION_IOBI=$AC_DIRECTORIO.'form/iobi/';
+
+		$verificar_carpeta=$NUEVA_UBICACION.'/pubdatos.php';
 		if(file_exists($verificar_carpeta)){ require_once $verificar_carpeta;
-			$ubicar=$AC_DIRECTORIO.$carpeta.'/datos/reportes/';
+			$ubicar=$NUEVA_UBICACION.'/reportes/';
 			date_default_timezone_set("America/Bogota");
     		$fecha=date("d M Y - g:ia");
 			$ex='Contador';
 			$UbicacionArchivoContador=$ubicar.'r'.$id.'.txt';
-			require_once $AC_DIRECTORIO.'datos/extenciones.php';
+			require $AC_DIRECTORIO.'datos/extenciones.php';
 
 			$leerTotal=file_get_contents($UbicacionArchivoContador);
 			$leerA=$ubicar.'rd'.$id.'.txt';
+			if(!file_exists($leerA)){ file_put_contents($leerA,''); }
 			$leerDatos=file_get_contents($leerA);
 
 			if ($leerTotal >= 4){
-				$UbicacionArchivoContador=$AC_DIRECTORIO.$carpeta.'/datos/estados/revision.txt';
-				include $AC_DIRECTORIO.'datos/extenciones.php';
+				$UbicacionArchivoContador=$NUEVA_UBICACION.'/estados/revision.txt';
+				require $AC_DIRECTORIO.'datos/extenciones.php';
 			}
 
 			file_put_contents($leerA,$leerDatos."N: $leerTotal\nC: $caso\nM: $motivos\nF: $fecha\n---->\n");
 			$AC_TITULO='Oh!. Gracias por reportar el comentario!';
 			$reporte=$_GET['id'];
-			$mostrar='<p class="texini">El reporte fue enviado exitosamente y será revisado lo más pronto posible.</p>
-					<p class="texini t14">'.$AC_DESCRIPCION_reportarexito.'</p>';
+			$mostrar='<p class="texinimen bgverde">El reporte fue enviado exitosamente y será revisado lo más pronto posible.</p>
+					<p class="texini t14">Las personas como tu son las que mejoran forolink, esperemos que seas de los buenos y me ayudes con los reportes de comentarios que incumplan las reglas de forolink.</p>';
 			$AC_CONTENIDO='<div class="izqcon">
 					'.$mostrar.'
-					<a class="boton" href="'.$AC_DIRECTORIOs.'">Regresar</a>
+					<a class="boton" href="'.$AC_DIRECTORIO.'">Regresar</a>
 					</div>';
 		}
 	}
-} 
-
+}
 $AC_CONTENIDO='<div class="izqcon">
 '.$mostrar.'
 <a class="boton" href="'.$AC_DIRECTORIOs.'">Regresar</a>
 </div>';
-
-include $AC_DIRECTORIO.'datos/displa.php';
+require_once $AC_DIRECTORIO.'datos/displa.php';
+$AC_EXISTE=$opcExiste;
+$AC_ESTADO=$opcEstado;
+#v0.3.1 Beta
 ?>
+<?php #2023-07-31 - 6:04pm ?>
