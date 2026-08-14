1b219cc69f15d9828d795d4be8c83ae5abfc46e3
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 15:32:03 -0500

    Enhance form handling in creador.php with improved file management and user feedback

--- administracion/panel/creador.php
+++ administracion/panel/creador.php
@@ -1,36 +1,69 @@
 <?php if(isset($TIPO)){ if($TIPO='panel'){
+if (isset($_GET['archiguar'])) {
+    echo '<p class="texini bgazul">Se guardo en el borrador #'.$_GET['archiguar'];
+}
+if (isset($_POST['IniModificar'])) {
+    $ModArchivo=$_POST['archivo'];
+    $UbiArchivo=$AC_DIRECTORIO.'datos/contenidos/'.$ModArchivo.'.php';
+    if (file_exists($UbiArchivo)) {
+        require_once $UbiArchivo;
+        $permiExtras=true;
+        $PermiEditar=true;    
+    } else {
+        $permiExtras=false;
+        $PermiEditar=false;
+        echo '<p class="texini bgrojo">El archivo no existe!</p>';
+    }
+}
 if(isset($_GET['di']) && $_GET['di'] == true){
-    $opc1=$_GET['opc1'];
-    $opc2=$_GET['opc2'];
-    $opc3=$_GET['opc3'];
-    $opc4=$_GET['opc4'];
-    $opc5=$_GET['opc5'];
-    $opc6=$_GET['opc6'];
-    $opc7=$_POST['opc7'];
-    $opc8=$_GET['opc8'];
-    $opc9=$_GET['opc9'];
-    $opc10=$_GET['opc10'];
-    $opc11=$_GET['opc11'];
-    $opc12=$_GET['opc12'];
+    $exP='exPMetodo'; $exP_Metodo='GET'; require 'extencionesPanel.php';
+
+    if (isset($_GET['ModArchivo'])) {
+        $ModArchivo=$_GET['ModArchivo'];
+        $UbiArchivo=$AC_DIRECTORIO.'datos/contenidos/'.$ModArchivo.'.php';
+    
+        if (file_exists($UbiArchivo)) {
+            require_once $UbiArchivo;
+            $opcModArchivo=$_GET['opcModArchivo'];
+            $permiExtras=true;
+            $PermiEditar=true;
+            switch ($opcModArchivo) {
+                case 'complementos': $opcModArchivoB='selected'; break;
+                case 'ambos': $opcModArchivoC='selected'; break;
+            }
+        } else {
+            $permiExtras=false;
+            $PermiEditar=false;
+            echo '<p class="texini bgrojo">El archivo no existe!</p>';
+        }
+        
+    }
     
     if (isset($_GET['opcBorrador'])) {
         switch ($_GET['opcBorrador']) {
             case 2: $opcBorradorB='selected'; break;
             case 3: $opcBorradorC='selected'; break;
         }
+        $permiExtras=true;
     }
-    
-    if ($opc8 == 'no') { $opc8N='selected'; }
+}
+
+if (isset($permiExtras) && $permiExtras == true) {
+    $exP='exPOpc8'; require_once 'extencionesPanel.php';
     switch ($opc9) {
         case 'foro': $opc9Foro='selected'; break;
+        case 'comentarios': $opc9Comentarios='selected'; break;
         case 'entradas': $opc9Entradas='selected'; break;
         case 'blog': $opc9Blog='selected'; break;
     }
     switch ($opc10) {
         case '../': $opc10B='selected'; break;
         case '../../': $opc10C='selected'; break;
+        case '../../../': $opc10D='selected'; break;
+        case '../../../../': $opc10E='selected'; break;
     }
 }
+
 ?>
 <p class="texini">Creemos algo epico!</p>
     <div class="flexCrear">
@@ -46,10 +79,11 @@ if(isset($_GET['di']) && $_GET['di'] == true){
             <textarea class="texeditor2" name="opc7" placeholder="Contenido" minlength="4" required><?php if(isset($opc7)){ echo $opc7; }; ?></textarea>
             <span>Anuncio</span> <select name="opc8">
                 <option value="si">Si</option>
-                <option value="no" <?php if(isset($opc8N)){ echo $opc8N; }; ?>>No</option>
+                <option value="no" <?php if(isset($_GET['opc8']) && $_GET['opc8']=='no'){ echo 'selected'; } ?>>No</option>
             </select>
             <span>Tipo</span> <select name="opc9">
                 <option value="normal">Normal</option>
+                <option value="comentarios" <?php if(isset($opc9Comentarios)){ echo $opc9Comentarios; }; ?>>Comentarios</option>
                 <option value="foro" <?php if(isset($opc9Foro)){ echo $opc9Foro; }; ?>>Foro</option>
                 <option value="entradas" <?php if(isset($opc9Entradas)){ echo $opc9Entradas; }; ?>>Entradas</option>
                 <option value="blog" <?php if(isset($opc9Blog)){ echo $opc9Blog; }; ?>>Blog</option>
@@ -58,10 +92,21 @@ if(isset($_GET['di']) && $_GET['di'] == true){
                 <option value="./">./</option>
                 <option value="../" <?php if(isset($opc10B)){ echo $opc10B; }; ?>>../</option>
                 <option value="../../" <?php if(isset($opc10C)){ echo $opc10C; }; ?>>../../</option>
+                <option value="../../../" <?php if(isset($opc10D)){ echo $opc10D; }; ?>>../../../</option>
+                <option value="../../../../" <?php if(isset($opc10E)){ echo $opc10E; }; ?>>../../../../</option>
             </select><br>
             <input type="text" value="<?php if(isset($opc11)){ echo $opc11; }; ?>" name="opc11" placeholder="Ubicación <opcional/>" minlength="4">
-            <input type="text" value="<?php if(isset($opc12)){ echo $opc12; }; ?>" name="opc12" placeholder="Nombre del archivo" minlength="4" required><hr>
-            <input type="submit" name="publicar" value="Publicar &#xf044">
+            <input type="text" value="<?php if(isset($opc12)){ echo $opc12; }; ?>" name="opc12" placeholder="Nombre del archivo" minlength="4" required>
+            <?php if (isset($PermiEditar) && $PermiEditar==true) { ?><hr>
+                <input type="text" name="ModArchivo" value="<?php echo $ModArchivo; ?>">
+                <select name="opcModArchivo">
+                <option value="datos">Datos</option>
+                <option value="complementos" <?php if(isset($opcModArchivoB)){ echo $opcModArchivoB; }; ?>>Complementos</option>
+                <option value="ambos" <?php if(isset($opcModArchivoC)){ echo $opcModArchivoC; }; ?>>Ambos</option>
+            </select>
+            <?php } ?>
+            <hr>
+            <input type="submit" name="publicar" value="Mostrar &#xf044">
         </form><hr>
         <form action="borrador.php" method="get">
             <span>Borradores</span>
@@ -87,7 +132,7 @@ if(isset($_GET['di']) && $_GET['di'] == true){
                 <li>&lt;p class="texini"&gt;Hola!&lt;/p&gt;</li>
                 <li>&lt;ol&gt; &lt;li class="t12"&gt;Información&lt;/li&gt; &lt;/ol&gt;</li>
                 <li><?php echo $AC_DESCRIPCION_creador; ?></li>
-                <li>Disfruta de la versión 0.2 Beta mejorada!</li>
+                <li>Disfruta de la versión 0.3 Beta mejorada!</li>
             </ol>
         </div>
     </div>
