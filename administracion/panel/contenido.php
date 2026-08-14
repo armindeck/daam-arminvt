4c194a6310644d1fae343736e4214808565e8be0
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 15:04:54 -0500

    Agregue la sección de modificador (modificar post), displadi (Plantilla) y tema, ademas modifique la vista de la sección de los archivos

--- administracion/panel/contenido.php
+++ administracion/panel/contenido.php
@@ -9,14 +9,17 @@ if(isset($_GET['ac'])){
 	switch($ac){
 		case 'verificar': require_once 'verificar.php'; break;
 		case 'creador': require_once 'creador.php'; break;
+		case 'modificador': require_once 'modificador.php'; break;
 
-		case 'archivos': echo '<form class="formulario" method="post" action="actualizar.php"><span>Eliminar archivo</span><br><input type="text" name="archivo" value="administracion/panel/creadas/"><input name="IniEliminarArchivo" type="submit" value="Eliminar &#xf1f8"></form><form class="formulario" method="post" action="actualizar.php"><span>Eliminar carpeta</span><br><input type="text" name="carpeta"><input name="IniEliminarCarpeta" type="submit" value="Eliminar &#xf1f8"></form><form class="formulario" method="post" action="actualizar.php"><span>Crear carpeta</span><br><input type="text" name="carpeta"><input name="IniCrearCarpeta" type="submit" value="Crear &#xf0fe"></form><form class="formulario" method="post" action="actualizar.php"><span>Crear archivo</span><br><input type="text" name="archivo"><input name="IniCrearArchivo" type="submit" value="Crear &#xf0fe"></form>'; break;
+		case 'archivos': echo '<p class="texini">Creador y eliminador de archivos y carpetas <span class="t14">v0.3 Beta</span></p><form class="formulario" method="post" action="actualizar.php"><span>Eliminar archivo</span><br><input type="text" name="archivo" value="administracion/panel/creadas/"><input name="IniEliminarArchivo" type="submit" value="Eliminar &#xf1f8"></form><form class="formulario" method="post" action="actualizar.php"><span>Eliminar carpeta</span><br><input type="text" name="carpeta"><input name="IniEliminarCarpeta" type="submit" value="Eliminar &#xf1f8"></form><form class="formulario" method="post" action="actualizar.php"><span>Crear carpeta</span><br><input type="text" name="carpeta"><input name="IniCrearCarpeta" type="submit" value="Crear &#xf0fe"></form><form class="formulario" method="post" action="actualizar.php"><span>Crear archivo</span><br><input type="text" name="archivo"><input name="IniCrearArchivo" type="submit" value="Crear &#xf0fe"></form>'; break;
 
 		case 'anuncios': require_once 'anuncios.php'; break;
 
 		case 'blog': require_once 'formblog.php'; break;
 
 		case 'configuracion': require_once 'configuraciones.php'; break;
+		case 'displadi': require_once 'displadi.php'; break;
+		case 'tema': require_once 'tema.php'; break;
 
 		case 'editor': require_once 'editor.php'; break;
 
