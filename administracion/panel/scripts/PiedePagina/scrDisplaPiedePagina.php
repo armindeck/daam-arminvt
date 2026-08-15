613831088858e451341a1acbad38076877a53bdb
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 20:23:54 -0500

    Movidos a la carpeta Scripts

--- administracion/panel/scripts/PiedePagina/scrDisplaPiedePagina.php
+++
@@ -1,5 +0,0 @@
-<?php if($i===1 && isset($PiedePaginaDerechos) && $PiedePaginaDerechos=="on"): ?>
-
-© <?php echo $Año.' '.$NombreWeb.'<br><span class="t12">Theme '.$version.' by <a target="_blank" href="https://dbproject.rf.gd/">Armin</a></span>'; ?>
-
-<?php endif; ?>
