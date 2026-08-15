e0e83813d57f9bf9762f5d9a89cd66d2df13914a
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 19:20:13 -0500

    Update content and timestamps in multiple PHP files for consistency and accuracy

--- datos/contenidos/cn_actualizaciones.php
+++ datos/contenidos/cn_actualizaciones.php
@@ -2,7 +2,7 @@
 $opc1='Actualizaciones';
 $opc2='Actualizaciones';
 $opc3='Actualizaciones';
-$opc4='miniatura.png';
+$opc4='arminvtcodigo.png';
 $opc5='Actualizaciones';
 $opc6='Actualizaciones';
 $opcXMensaje='on';
@@ -14,7 +14,7 @@ $opc9='blog';
 $opc10='./';
 $opc11='';
 $opc12='actualizaciones';
-$fecha="2023-08-23 - 4:11pm";
+$fecha="2024-01-24 - 10:01am";
 $opcEstado='publico';
 $opcExiste=true;
 #v0.3.1 Beta
--- datos/contenidos/cn_index.php
+++ datos/contenidos/cn_index.php
@@ -8,13 +8,13 @@ $opc6='arminvt es un sitio web donde puede divertirte comentando en los forolink
 $opcXMensaje='';
 $opcXAccesoAdmin='';
 $opcXGaleria='';
-$opc7='<p class="texinimen bgrojo">Se encuentra disponible la versión 0.3.3 Beta! y llega con nuevas mejoras!</p><p class="texini">Hola!, soy <a target="_blank" href="https://dbproject.rf.gd/">Armin</a> y soy vtuber, desarrollador de sitios web y juegos que comparto en mi <a target="_blank" href="https://youtube.com/@SoyArminDeck">Canal de YouTube</a>. Además me gusta dibujar, editar y crear videos, los cuales son subidos al canal. Desarrollo páginas web como la presente <span class="tb">arminvt</span> donde subo mis contenidos de información y actualizaciones de ciertos temas. <a target="_blank" href="https://dbproject.rf.gd/">¡Sigue a delante!</a></p><div class="cen"><a class="boton2" target="_blank" href="https://youtube.com/@SoyArminDeck?sub_confirmation=1"><i class="fab fa-youtube"></i> Suscribete</a><a class="boton" target="_blank" href="iniciar">Iniciar <i class="fas fa-user"></i></a><a class="boton2" target="_blank" href="forolink">ForoLink <i class="fas fa-fire"></i></a>	</div><hr>	<p class="titulo">Últimos proyectos</p><hr>';
+$opc7='<p class="texinimen bgrojo">Se encuentra disponible la versión 0.3.4 Beta! y llega con nuevas mejoras!</p><p class="texini">Hola!, soy <a target="_blank" href="https://dbproject.rf.gd/">Armin</a> y soy vtuber, desarrollador de sitios web y juegos que comparto en mi <a target="_blank" href="https://youtube.com/@soyarmindeck">Canal de YouTube</a>. Además me gusta dibujar, editar y crear videos, los cuales son subidos al canal. Desarrollo páginas web como la presente <span class="tb">arminvt</span> donde subo mis contenidos de información y actualizaciones de ciertos temas. <a target="_blank" href="https://dbproject.rf.gd/">¡Sigue a delante!</a></p><div class="cen"><a class="boton2" target="_blank" href="https://youtube.com/@soyarmindeck?sub_confirmation=1"><i class="fab fa-youtube"></i> Suscribete</a><a class="boton" target="_blank" href="iniciar">Iniciar <i class="fas fa-user"></i></a><a class="boton2" target="_blank" href="forolink">ForoLink <i class="fas fa-fire"></i></a>	</div><hr>	<p class="titulo">Últimos proyectos</p><hr>';
 $opc8='si';
 $opc9='entradas';
 $opc10='./';
 $opc11='';
 $opc12='index';
-$fecha="2024-09-29 - 11:34am";
+$fecha="2024-01-18 - 6:04pm";
 $opcEstado='publico';
 $opcExiste=true;
 #v0.3.1 Beta
