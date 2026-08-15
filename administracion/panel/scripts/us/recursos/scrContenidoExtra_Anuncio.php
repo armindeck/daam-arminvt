a8427e313351fa47e475d79c7859aae64ac4ffdb
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 20:21:53 -0500

    Add scrContenidoExtra_Anuncio.php for content display and sharing options

---
+++ administracion/panel/scripts/us/recursos/scrContenidoExtra_Anuncio.php
@@ -0,0 +1,14 @@
+<div class="m2">
+	<a target="_blank" href="<?php echo isset($scrUS_ContenidoExtra_Enlace) ? $scrUS_ContenidoExtra_Enlace : ''; ?>">
+		<div class="imagen">
+			<img class="img1" src="<?php echo isset($scrUS_ContenidoExtra_Enlace_Imagen) ? $scrUS_ContenidoExtra_Enlace_Imagen : ''; ?>">
+		</div>
+		<p class="contexcn cen"><?php echo isset($scrUS_ContenidoExtra_Contenido) ? $scrUS_ContenidoExtra_Contenido : ''; ?></p>
+	</a>
+	<div class="titulo cen"><span class="t18">Comparte el episodio papu!</span><br><span class="t25">
+		<a href="https://facebook.com/sharer.php?u=<?php echo $URL; ?>" title="Compartir en Facebook" rel="nofollow" target="_blank" href=""><i class="fab fa-facebook"></i></a>
+		<a href="https://twitter.com/share?url=<?php echo $URL; ?>" title="Compartir en Twitter" rel="nofollow" target="_blank" href=""><i class="fab fa-twitter"></i></a>
+		<a href="https://t.me/share/url?url=<?php echo $URL; ?>" title="Compartir en Telegram" rel="nofollow" target="_blank" href=""><i class="fab fa-telegram"></i></a>
+		<a href="https://api.whatsapp.com/send?text=<?php echo $URL; ?>" title="Compartir en WhatsApp" rel="nofollow" target="_blank" href=""><i class="fab fa-whatsapp"></i></a></span>
+	</div>
+</div></div>
