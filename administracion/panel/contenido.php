1547bf0440fd160c474813ec267ca245103f75aa
armindeck <dbhsoficial2003@gmail.com>
2026-08-14 20:06:46 -0500

    Fix require paths in contenido.php for better file structure

--- administracion/panel/contenido.php
+++ administracion/panel/contenido.php
@@ -1,24 +1,18 @@
 <?php if(isset($TIPO) && $TIPO=='panel'){
-
 if(isset($_GET['ac'])){
-
 	$ac=$_GET['ac'];
-
 	switch($ac){
-		case 'verificar': require_once 'verificar.php'; break;
-		case 'creador': require_once 'creador.php'; break;
-		case 'imagen': require_once 'imagen.php'; break;
-		case 'archivos': require_once 'archivos.php'; break;
-		case 'anuncios': require_once 'anuncios.php'; break;
-		case 'configuracion': require_once 'configuraciones.php'; break;
-		case 'displadi': require_once 'displadi.php'; break;
-		case 'tema': require_once 'tema.php'; break;
-		case 'editor': require_once 'editor.php'; break;
+		case 'creador': require_once 'creador/creador.php'; break;
+		case 'imagen': require_once 'imagen/imagen.php'; break;
+		case 'archivos': require_once 'archivos/archivos.php'; break;
+		case 'anuncios': require_once 'anuncios/anuncios.php'; break;
+		case 'configuracion': require_once 'configuraciones/configuraciones.php'; break;
+		case 'displadi': require_once 'displadi/displadi.php'; break;
+		case 'tema': require_once 'tema/tema.php'; break;
+		case 'editor': require_once 'editor/editor.php'; break;
 
 		default: echo 'Oh! no existe el get ingresado...'; break;
-
 	}
-
 }
 } else {
     if(isset($AC_DIRECTORIO)){ $AC_DIRECTORIO=$AC_DIRECTORIO; } else { $AC_DIRECTORIO='../../'; }
