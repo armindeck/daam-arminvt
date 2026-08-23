29c36737ad0e2b1416bd7dbf6cc10fe259a362d9
armindeck <dbhsoficial2003@gmail.com>
2026-08-23 00:20:11 -0500

    Refactor configuraciones.php para mejorar la estructura del formulario y agregar nuevas opciones de configuración

--- administracion/panel/configuraciones/configuraciones.php
+++ administracion/panel/configuraciones/configuraciones.php
@@ -1,61 +1,52 @@
-<?php if(isset($TIPO) && $TIPO=='panel'){ ?>
-	<p class="texini">Configuración avanzada <span class="t12"><?php echo file_get_contents(__DIR__.'/configuraciones.x'); ?></span></p>
-<form class="flexCon" method="post" action="configuraciones/procesa.php">
-	<div class="formulario">
-	<p class="tb">Nombres</p><hr>
-	<span>Admin:</span> <input type="text" name="cfg0" value="<?php echo $NombreAdmin; ?>" placeholder="Admin"><br>
-	<span>Admin Completo:</span> <input type="text" name="cfg1" value="<?php echo $NombreAdminCompleto; ?>" placeholder="Admin Completo"><br>
-	<span>Web:</span> <input type="text" name="cfg2" value="<?php echo $NombreWeb; ?>" placeholder="Web"><br>
-	<span>Facebook:</span> <input type="text" name="cfg3" value="<?php echo $NombreFacebook; ?>" placeholder="Facebook"><br>
-	<span>YouTube:</span> <input type="text" name="cfg4" value="<?php echo $NombreYouTube; ?>" placeholder="YouTube"><br>
-	<span>Twitter:</span> <input type="text" name="cfg5" value="<?php echo $NombreTwitter; ?>" placeholder="Twitter"><br>
-	<span>Patreon:</span> <input type="text" name="cfg6" value="<?php echo $NombrePatreon; ?>" placeholder="Patreon"><br>
-	<span>Tiktok:</span> <input type="text" name="cfg7" value="<?php echo $NombreTiktok; ?>" placeholder="Tiktok"><br>
-	<span>Kofi:</span> <input type="text" name="cfg8" value="<?php echo $NombreKofi; ?>" placeholder="Kofi">
-	</div>
-	<div class="formulario">
-	<p class="tb">Usuarios</p><hr>
-	<span>Admin:</span> <input type="text" name="cfg9" value="<?php echo $UsuarioAdmin; ?>" placeholder="Admin"><br>
-	<span>Facebook:</span> <input type="text" name="cfg10" value="<?php echo $UsuarioFacebook; ?>" placeholder="Facebook"><br>
-	<span>YouTube:</span> <input type="text" name="cfg11" value="<?php echo $UsuarioYouTube; ?>" placeholder="YouTube"><br>
-	<span>Twitter:</span> <input type="text" name="cfg12" value="<?php echo $UsuarioTwitter; ?>" placeholder="Twitter"><br>
-	<span>Patreon:</span> <input type="text" name="cfg13" value="<?php echo $UsuarioPatreon; ?>" placeholder="Patreon"><br>
-	<span>Tiktok:</span> <input type="text" name="cfg14" value="<?php echo $UsuarioTiktok; ?>" placeholder="Tiktok"><br>
-	<span>Kofi:</span> <input type="text" name="cfg15" value="<?php echo $UsuarioKofi; ?>" placeholder="Kofi"><hr>
-	<p class="tb">Enlaces</p><hr>
-	<span>Web:</span> <input type="url" name="cfg16" value="<?php echo $EnlaceWeb; ?>" placeholder="http://web.com"><br>
-	<span>Web no https:</span> <input type="text" name="cfg17" value="<?php echo $EnlaceWebNoHttps; ?>" placeholder="web.com"><br>
-	<span>Admin: </span> <input type="url" name="cfgadminexterno" placeholder="Enlace Admin" value="<?php
-	if($EnlaceAdmin!="$AC_DIRECTORIOs$UsuarioAdmin$AGREGAR_PHP"){ echo $EnlaceAdmin; }
-	?>"><br>
-	<span>LocalHost:</span> <input type="text" name="cfglocalhost" value="<?php echo $LocalHost; ?>" placeholder="paginaweb"><br>
-	<span>Publicada:</span> <input type="number" name="cfg_anio_publicada" value="<?php echo $AnioPublicada; ?>" placeholder="Año publicada"><hr>
-	<span>Versión</span> <input type="text" name="cfgversion" value="<?php echo $version; ?>" placeholder="v0.3 Beta"><hr>
-		<div class="flexRow">
-			<span>Activar .PHP:</span>
-			<div class="der">
-				<input type="checkbox" name="cfg18" <?php if($AGREGAR_PHP=='.php'){ echo 'checked'; } ?>>
-			</div>
-		</div><hr>
-		<div class="flexRow">
-			<span>Activar HTTPS:</span>
-			<div class="der">
-				<input type="checkbox" name="cfg19" <?php if($EnlaceWebS=='https://'.$EnlaceWebNoHttps.'/'){ echo 'checked'; } ?>>
-			</div>
-		</div>
-	</div>
-	<div class="formulario" style="width:100%;">
-		<span>Extras</span> <textarea name="cfg20" placeholder="Variables y resultados extras"><?php if(file_exists($scrSecundarios)){ $mostrar=file_get_contents($scrSecundarios); echo $mostrar; } ?></textarea>
-	</div>
-	<div class="formulario" style="width:100%;">
-		<span>Scripts</span> <textarea class="texeditor2" name="cfg21" placeholder="Analitycs"><?php if(file_exists($scrExtrasdispla)){ $mostrar=file_get_contents($scrExtrasdispla); echo $mostrar; } ?></textarea>
-	</div>
-	<div>
-        <input class="boton" type="reset" value="Cancelar &#xf00d">
-        <input class="boton" type="submit" name="IniConfig" value="Actualizar &#xf044">
+<?php
+
+$AC_DIRECTORIO = $AC_DIRECTORIO ?? "../../";
+if(!isset($TIPO) || $TIPO != "panel"){
+  redirect($AC_DIRECTORIO."error.php?ms=err&msm=accdenegado");
+}
+
+?>
+<div class="flex flex-evenly">
+  <form method="post" action="actualizar.php" style="width: 100%; max-width: 720px;">
+    <div class="formulario" style="width: 99%;">
+      <div class="p-4 t-strong">
+        Configuraciones
+      </div>
+      <div class="flex flex-column gap-2">
+		<label for="page_name">Nombre de la pagina:</label>
+        <input type="text" name="page_name" id="page_name" placeholder="Nombre" value="<?= CONFIG["config"]["page_name"] ?? "" ?>">
+		<label for="page_link">Enlace de la pagina:</label>
+        <input type="url" name="page_link" id="page_link" placeholder="Enlace" value="<?= CONFIG["config"]["page_link"] ?? "" ?>">
+		<label for="page_year">Año publicada:</label>
+        <input type="number" min="1900" max="5000" name="page_year" id="page_year" placeholder="Año" value="<?= CONFIG["config"]["page_year"] ?? "" ?>">
+		<label for="page_about">Acerca de:</label>
+        <textarea class="textarea-full" rows="5" name="page_about" placeholder="Acerca de la pagina"><?= CONFIG["config"]["page_about"] ?? "" ?></textarea>
+		<label for="page_tags">Etiquetas:</label>
+        <textarea class="textarea-full" rows="3" name="page_tags" placeholder="Etiquetas, de, la, pagina"><?= CONFIG["config"]["page_tags"] ?? "" ?></textarea>
+		<label for="page_scripts">Scripts:</label>
+        <textarea class="textarea-full" rows="10" name="page_scripts" placeholder="<?= secureString("<!-- Google Ads/Analitycs, Font Awesome, Other... -->") ?>"><?= secureString(CONFIG["config"]["page_scripts"] ?? "") ?></textarea>
+		<label class="flex flex-between gap-4 items-center">
+			<span>Habilitar Scripts:</span>
+			<?= viewSelect("page_scripts_active", [["" => "No"], ["1" => "Si"]], CONFIG["config"]["page_scripts_active"] ?? "") ?>
+		</label>
+		<label class="flex flex-between gap-4 items-center">
+			<span>Habilitar SSL:</span>
+			<?= viewSelect("page_ssl_active", [["" => "No"], ["1" => "Si"]], CONFIG["config"]["page_ssl_active"] ?? "") ?>
+		</label>
+		<label class="flex flex-between gap-4 items-center">
+			<span>Habilitar extension .PHP:</span>
+			<?= viewSelect("page_extension_php_active", [["" => "No"], ["1" => "Si"]], CONFIG["config"]["page_extension_php_active"] ?? "") ?>
+		</label>
+		<label class="flex flex-between gap-4 items-center">
+			<span>⚠️ Modo DEBUG:</span>
+			<?= viewSelect("page_debug_active", [["" => "No"], ["1" => "Si"]], CONFIG["config"]["page_debug_active"] ?? "") ?>
+		</label>
+      </div>
+      <hr>
+      <div class="flex flex-between p-8">
+        <input class="boton2" type="reset" value="Cancelar">
+        <input class="boton" type="submit" name="IniConfig" value="Actualizar">
+      </div>
     </div>
-</form>
-<?php } else {
-    if(isset($AC_DIRECTORIO)){ $AC_DIRECTORIO=$AC_DIRECTORIO; } else { $AC_DIRECTORIO='../../'; }
-    $vamos=$AC_DIRECTORIO."error.php?ms=err&msm=accdenegado"; header("Location: {$vamos}");
-} ?>
+  </form>
+</div>
