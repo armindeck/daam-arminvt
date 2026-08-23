f86723104e041c19c40913b1feeb5c69163b1e97
armindeck <dbhsoficial2003@gmail.com>
2026-08-23 00:14:59 -0500

    Eliminar lógica redundante en la actualización de anuncios y requerir el procesamiento desde un archivo externo

--- administracion/panel/actualizar_acc.php
+++ administracion/panel/actualizar_acc.php
@@ -172,31 +172,7 @@ if(isset($_FILES)){
 #ACTUALIZAR ANUNCIOS ----------------------->
 
 if ($_POST['IniAnuncio']){
-
-    $sms=$_POST['mensaje'];
-
-    $enlace=$_POST['enlace'];
-
-    $enlaceanuncio=$_POST['anuncio'];
-
-    $enlaceanuncio2=$_POST['anuncio2'];
-
-    $linkimga=$_POST['imga'];
-
-    $linkimga2=$_POST['imga2'];
-
-    $texMensaje=$_POST['texMensaje'];
-
-    $mosAnuncio=$_POST['mosAnuncio'];
-
-    $mosAnuncio2=$_POST['mosAnuncio2'];
-
-
-
-    file_put_contents('dependencias/dpanuncio.php',"<?php\n".'$sms='."'$sms';\n".'$link='."'$enlace';\n".'$linkanuncio='."'$enlaceanuncio';\n".'$linkanuncio2='."'$enlaceanuncio2';\n".'$linkimga='."'$linkimga';\n".'$linkimga2='."'$linkimga2';\n".'$texMensaje='."'$texMensaje';\n".'$mosAnuncio='."'$mosAnuncio';\n".'$mosAnuncio2='."'$mosAnuncio2';\n?>");
-
-    header("location:panel.php?ac=anuncios&ms=exi&msm=datosactualizados");
-
+	require_once __DIR__."/anuncios/procesa.php";
 } else
 
 
