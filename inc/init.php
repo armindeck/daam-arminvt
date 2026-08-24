9798341d25fda42f48d6980c0376362ca7e8acdb
armindeck <dbhsoficial2003@gmail.com>
2026-08-23 23:55:43 -0500

    Refactor la inicialización de la aplicación y mejora la verificación de autenticación

--- inc/init.php
+++ inc/init.php
@@ -1,8 +1,37 @@
 <?php
 
-require_once __DIR__."/function.php";
+require_once __DIR__ . "/function.php";
+require_once __DIR__ . "/function-deprecated.php";
 
 generateFilesData();
 
-define("CORE", readJson(pathData()."/core.json"));
-define("CONFIG", readJson(pathData()."/config.json"));
+define("CORE", readJson(pathDataCore()));
+define("CONFIG", readJson(pathDataConfig()));
+define("ALERTS", readJson(pathDataAlerts()));
+define("VISITS", readJson(pathDataVisits()));
+define("TIMEZONE", readJson(pathDataTimezone()));
+define("USERS", readJson(pathDataUsers()));
+define("POSTS", readJson(pathDataPosts()));
+
+session_start([
+  "cookie_secure" => true, // Solo HTTPS
+  "cookie_httponly" => true, // No accesible desde JS
+  "cookie_samesite" => "lax", // Protección CSRF
+  "use_strict_mode" => true // Evita session fixation
+]);
+
+date_default_timezone_set(CONFIG["page_timezone"] ?? "America/Bogota");
+error_reporting(CONFIG["page_debug_active"] ?? false);
+
+# AUTH VERIFY
+if (auth() && !authVerify(USERS)) {
+  if (logout()) redirect("./iniciar.php");
+  redirect("./error");
+}
+
+if (!empty($_GET["logout"])) {
+  if (logout()) redirect("./iniciar.php?ms=exi&msm=sesionfinalizada");
+  redirect("./error");
+}
+
+#setVisits("/blog/los-mejores-momentos-del-2026", VISITS);
