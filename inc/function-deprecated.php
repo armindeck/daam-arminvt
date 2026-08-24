55c7153d6da2c39df82b183f83e2ab84b807ed2c
armindeck <dbhsoficial2003@gmail.com>
2026-08-24 01:05:16 -0500

    Agregar funciones de formato de texto y limpieza de caracteres especiales en function-deprecated.php para mejorar la manipulación de cadenas.

--- inc/function-deprecated.php
+++ inc/function-deprecated.php
@@ -1,5 +1,56 @@
 <?php # FUNCIONES DEPRECATES
 
+# ----------- DEPRECATED ---------------
+
+function darFormato($string)
+{
+  $string = str_replace(array('<'), '«', $string);
+  $string = str_replace(array('>'), '»', $string);
+  $string = str_replace(array("'"), ' &#039; ', $string);
+  $string = str_replace(array('"'), ' &quot; ', $string);
+  $string = str_replace(array('{'), ' &#123; ', $string);
+  $string = str_replace(array('}'), ' &#125; ', $string);
+  $string = str_replace(array('#BR#'), ' <br> ', $string);
+  $string = str_replace(array('#BA#'), ' <b> ', $string);
+  $string = str_replace(array('#BC#'), ' </b> ', $string);
+  $string = str_replace(array('#IA#'), ' <i> ', $string);
+  $string = str_replace(array('#IC#'), ' </i> ', $string);
+  $string = str_replace(array('#HR#'), ' <hr> ', $string);
+  $string = str_replace(array('#TA#'), ' <span> ', $string);
+  $string = str_replace(array('#TA18#'), ' <span class="t18"> ', $string);
+  $string = str_replace(array('#TA14#'), ' <span class="t14"> ', $string);
+  $string = str_replace(array('#TA12#'), ' <span class="t12"> ', $string);
+  $string = str_replace(array('#TC#'), ' </span> ', $string);
+  return $string;
+}
+
+function darFormatoNoSimbolos($string)
+{
+  $string = str_replace(array('☺', '☻', '♥', '♦', '♣', '♠', '•', '◘', '○', '◙', '♂', '♀', '♪', '♫', '☼', '►', '◄', '↕', '‼', '¶', '§', '▬', '↨', '↑', '↓', '→', '←', '∟', '↔', '▲', '▼', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~', '⌂', 'ª', 'º', '¿', '®', '¬', '½', '¼', '¡', '«', '»', '░', '▒', '▓', '│', '┤', '©', '╣', '║', '╗', '╝', '¢', '¥', '┐', '└', '‼', '┴', '┬', '├', '─', '┼', '╚', '╔', '╩', '╦', '╠', '═', '╬', '¤', 'ð', '┘', '┌', '█', '▄', '¦', '▀', '¯', '´', '±', '³', '²', '¶', '§', '÷', '¸', '°', '¨', '·', '¹', '³', '²', '■', "'", '“', '”'), '', $string);
+  return $string;
+}
+
+function darFormatoIobi($string): string
+{
+  return str_replace('/', '-', $string);
+}
+
+function darFormatoConTXT($string): string
+{
+  $string = str_replace(['.php', '.css'], '.txt', $string);
+  $string = str_replace('.htaccess', '.htaccess.txt', $string);
+  return $string;
+}
+
+function darFormatoURL_FINAL($string, $LH): string
+{
+  $string = str_replace("$LH", '', $string);
+  $string = str_replace('&theme=Light', '', $string);
+  $string = str_replace('&theme=Dark', '', $string);
+  return $string;
+}
+
+
 /* ----------- EDITOR|EXPLORER ------------- */
 function LocalBuscar($buscar){
 	return glob($buscar, GLOB_BRACE);
