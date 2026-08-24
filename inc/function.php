362c1606b604c6d13ff7cc39ded8685610af6d51
armindeck <dbhsoficial2003@gmail.com>
2026-08-23 23:54:58 -0500

    Agregar funciones para manejar rutas de datos y autenticación de usuarios

--- inc/function.php
+++ inc/function.php
@@ -1,5 +1,6 @@
 <?php
 
+# -------- PATHS --------
 function raiz(): string
 {
   return __DIR__ . "/..";
@@ -8,6 +9,41 @@ function raiz(): string
 function pathData(): string
 {
   return raiz() . "/data";
+}
+
+function pathDataConfig(): string
+{
+  return pathData() . "/config.json";
+}
+
+function pathDataCore(): string
+{
+  return pathData() . "/core.json";
+}
+
+function pathDataTimezone(): string
+{
+  return pathData() . "/timezone.json";
+}
+
+function pathDataAlerts(): string
+{
+  return pathData() . "/alerts.json";
+}
+
+function pathDataVisits(): string
+{
+  return pathData() . "/visits.json";
+}
+
+function pathDataUsers(): string
+{
+  return pathData() . "/users.json";
+}
+
+function pathDataPosts(): string
+{
+  return pathData() . "/posts.json";
 }
 
 function secureString(string $string): string
@@ -35,7 +71,7 @@ function readJson(string $file_path): array
 
 function generateFilesData(): void
 {
-  foreach (["core", "config"] as $value) {
+  foreach (["core", "config", "timezone", "alerts", "visits", "users", "posts"] as $value) {
     if (!file_exists(pathData() . "/$value.json"))
       writeJson(pathData() . "/$value.json", []);
   }
@@ -59,6 +95,203 @@ function imagesListAndBaseName(): array
   return array_map(fn($images) => ["complete" => $images, "base" => basename($images)], imagesList());
 }
 
+function auth(): bool
+{
+  return isset($_SESSION["user_id"]) && isset($_SESSION["user_rol"]);
+}
+
+function authVerify(array $users): bool
+{
+  if (!auth()) return false;
+
+  $user = userLoginSearch($users);
+  return !empty($user) && $user["value"]["is_active"];
+}
+
+function isAdmin(): bool
+{
+  return auth() && $_SESSION["user_rol"] == "admin";
+}
+
+function login(string $username_ord_email, string $password, array $users): bool
+{
+  $user = userSearchByUserNameOrdEmail($username_ord_email, $users, false);
+  if (empty($user) || !$user["value"]["is_active"]) return false;
+
+  $users[$user["key"]]["date_last_login"] = dateTime();
+  $users[$user["key"]]["history"][] = ["login", dateTime()];
+  if (writeJson(pathDataUsers(), $users)) {
+    $_SESSION["user_id"] = $user["value"]["user_id"];
+    $_SESSION["user_rol"] = $user["value"]["user_rol"];
+    return true;
+  }
+
+  return false;
+}
+
+function userSearchById(int $user_id, array $users, bool $data_only = true): array
+{
+  foreach ($users as $key => $user) {
+    if ($user_id == $user["user_id"]) {
+      return $data_only ? $user : ["key" => $key, "value" => $user];
+    }
+  }
+
+  return [];
+}
+
+function userSearchByUserName(string $username, array $users, bool $data_only = true): array
+{
+  foreach ($users as $key => $user) {
+    if (strtolower($username) == strtolower($user["username"])) {
+      return $data_only ? $user : ["key" => $key, "value" => $user];
+    }
+  }
+
+  return [];
+}
+
+function userSearchByEmail(string $email, array $users, bool $data_only = true): array
+{
+  foreach ($users as $key => $user) {
+    if (strtolower($email) == strtolower($user["email"])) {
+      return $data_only ? $user : ["key" => $key, "value" => $user];
+    }
+  }
+
+  return [];
+}
+
+function userSearchByUserNameOrdEmail(string $username_ord_email, array $users, bool $data_only = true, bool $primary_searched = true, string $email = ""): array
+{
+  $searched = [];
+  $primary = [];
+  foreach ($users as $key => $user) {
+    if (strtolower($username_ord_email) == strtolower($user["username"])) {
+      $searched["username"] = $data_only ? $user : ["key" => $key, "value" => $user];
+      if ($primary_searched) return $searched["username"];
+    }
+
+    if (strtolower($email ?? $username_ord_email) == strtolower($user["email"])) {
+      $searched["email"] = $data_only ? $user : ["key" => $key, "value" => $user];
+      if ($primary_searched) return $searched["email"];
+    }
+
+    if (!$primary_searched && !empty($searched["username"]) && !empty($searched["email"])) {
+      return $searched;
+    }
+  }
+
+  return $searched;
+}
+
+function userLoginSearch(array $users, bool $data_only = true): array
+{
+  if (!auth()) return [];
+  return userSearchById($_SESSION["user_id"], $users, $data_only);
+}
+
+function register(string $username, string $name, string $email, string $password, string $password_confirm, array $users): array
+{
+  $search = userSearchByUserNameOrdEmail($username, $users, false, false, $email);
+  if (!empty($search)) {
+    $isset_username = isset($search["username"]);
+    $isset_email = isset($search["email"]);
+
+    $message = $isset_username && $isset_email ? "username_and_email_exists" : (
+      $isset_username ? "username_exists" : ($isset_email ? "email_exists" : "")
+    );
+
+    return [
+      "success" => false,
+      "message" => $message
+    ];
+  }
+
+  if ($password != $password_confirm) return ["success" => false, "message" => "password_is_diferent"];
+
+  $id = count($users);
+
+  $users[$id] = [
+    "user_id" => $id,
+    "username" => secureString($username),
+    "name" => secureString($name),
+    "password" => passwordHash($password),
+    "email" => strtolower(secureString($email)),
+    "avatar" => "",
+    "rol" => "user",
+    "is_active" => true,
+    "state" => "active",
+    "recovery_code" => generatePin(),
+    "date_last_login" => "",
+    "date_registered" => dateTime(),
+    "history" => []
+  ];
+
+  if (writeJson(pathDataUsers(), $users)) {
+    return login($username, $password, $users) ? [
+      "success" => true,
+      "message" => "account_create_and_login"
+    ] : [
+      "success" => false,
+      "message" => "account_create_not_login"
+    ];
+  }
+
+  return [
+    "success" => false,
+    "message" => "error_create_account"
+  ];
+}
+
+function logout(): bool
+{
+  unset($_SESSION["user_id"]);
+  unset($_SESSION["user_rol"]);
+  return true;
+}
+
+function passwordHash(string $password): string
+{
+  return passwordHash($password);
+}
+
+function passwordVerify(string $password, string $passwordHash): string
+{
+  return passwordVerify($password, $passwordHash);
+}
+
+function dateTime(): string
+{
+  return date("Y-m-d H:i:s");
+}
+
+function is_par_letter($numero)
+{
+  $letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "K", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  //$letras = ['A','E','I','O','U'];
+  return ($numero % 2 == 0) ? $letras[rand(0, count($letras) - 1)] : $numero;
+}
+
+function generatePin(array $cantidad = [4, 5, 7]): string
+{
+  $numeros = '';
+  foreach ($cantidad as $key => $valor) {
+    $numeros .= $key >= 1 ? '-' : '';
+    for ($i = 0; $i < $valor; $i++) {
+      $numeros .= is_par_letter(rand(0, 9));
+    }
+  }
+
+  return $numeros;
+}
+
+function setVisits(string $slug, array $visits): bool
+{
+  $visits[$slug] = ($visits[$slug] ?? 0) + 1;
+  $visits["total"] = ($visits["total"] ?? 0) + 1;
+  return writeJson(pathDataVisits(), $visits);
+}
 
 # ------------- ViewsComponents --------------
 
