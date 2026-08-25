3df87ce2e7a2e8f36fd70c72f4945986393e8a3a
armindeck <dbhsoficial2003@gmail.com>
2026-08-25 01:20:23 -0500

    Eliminar código innecesario y simplificar la inclusión de archivos en datos.php

--- datos/datos.php
+++ datos/datos.php
@@ -1,8 +1,2 @@
 <?php
-
 require_once __DIR__."/../inc/init.php";
-$adminprivado = require_once __DIR__.'/admin_data.php';
-
-require_once $AC_DIRECTORIO.'datos/mensajes.php';
-$ex='CargarTema';
-require_once $AC_DIRECTORIO.'datos/extenciones.php';
