7b6556a02487015fa09553b759095e990ab0575e
armindeck <dbhsoficial2003@gmail.com>
2026-08-25 01:28:27 -0500

    Script para adaptar los nuevos datos de los posts a la extructura antigua

---
+++ inc/scripts/scrPosts.php
@@ -0,0 +1,26 @@
+<?php
+
+if(isset($AC_DIRECTORIO)) return;
+if(!isset($id)) die("Posts no identificado");
+
+$slug = "/" . (ltrim(str_replace(".php", "", $id[1]), "/"));
+$post_data = postSearchBySlug($slug, POSTS);
+
+$AC_DIRECTORIO = $id[0];
+
+$acceso = true;
+
+$AC_UBICACION = !empty($id[1]) ? str_replace("./", "", rtrim(dirname($id[1]), "/") . "/") : "";
+$AC_ARCHIVO = basename($id[1]);
+$AC_METADESCRIPCION = $post_data["fragment"] ?? "";
+$AC_METADESCRIPCION2 = $post_data["fragment"] ?? "";
+$AC_METAETIQUETA = $post_data["tags"] ?? "";
+$AC_IMG = $post_data["image"] ?? "";
+$AC_EXTRA = $post_data["tags"] ?? "";
+$AC_TITULO = $post_data["title"] ?? "";
+$AC_CATALOGO = $post_data["catalog"] ?? "";
+$AC_DESCRIPCION = $post_data["fragment"] ?? "";
+$AC_FECHA = $post_data["date_last_updated"] ?? "";
+$AC_CONTENIDO = $post_data["content"] ?? "";
+$TIPO = !empty($post_data["type"]) ? ($post_data["type"] == "admin" ? "panel" : $post_data["type"]) : "";
+require_once $id[0].'datos/displa.php';
