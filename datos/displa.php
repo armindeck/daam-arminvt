05ba9c0dc3e2b5695d0af061614b65ad72fd2998
armindeck <dbhsoficial2003@gmail.com>
2026-08-25 01:24:17 -0500

    Elimine el codigo de cambiar tema, cambie la url ac por sc para las secciones del panel y optimice el codigo de lanzar la alerta

--- datos/displa.php
+++ datos/displa.php
@@ -39,38 +39,6 @@ require $AC_DIRECTORIO . 'datos/extenciones.php';
   <meta name="twitter:image" content="<?php echo (CONFIG["page_link"] ?? "") . '/img/' . $AC_IMG; ?>">
   <meta name="keywords" content="<?php echo ($AC_METADESCRIPCION . ", " . $AC_METADESCRIPCION2 . ", " . $AC_METAETIQUETA . ", " . date("Y") . ", " . (CONFIG["page_name"] ?? "") . ", " .  (CONFIG["page_link"] ?? "")); ?>">
   <?= !empty(CONFIG["page_scripts_active"]) ? CONFIG["page_scripts"] ?? "" : "" ?>
-  <?php
-  if (isset($_GET['temamodificado']) && $_GET['temamodificado'] == true && isset($_GET['temamodificadoarc'])) {
-    $arc = $_GET['temamodificadoarc'];
-    $diri = $AC_DIRECTORIO . 'css/' . $arc;
-    if (file_exists($diri)) {
-      file_put_contents($AC_DIRECTORIO . 'css/tmmod.php', '<?php $mod=true; $arc="' . $arc . '"; ?>');
-      require $diri;
-      $sp = true;
-    }
-  }
-  $ad58 = $AC_DIRECTORIO . 'css/tmmod.php';
-  if (file_exists($ad58)) {
-    require_once $ad58;
-    if ($mod == true && $arc != '') {
-      if (file_exists($AC_DIRECTORIO . '/css/' . $arc)) {
-        require_once $AC_DIRECTORIO . 'css/' . $arc;
-        $sp = true;
-      }
-    }
-  }
-  if (isset($_GET['temamodificadono'])) {
-    if (file_exists($ad58)) {
-      $sp = false;
-      unlink($ad58);
-    }
-  }
-  if (isset($sp) && $sp == true) {
-    $tm = $AC_DIRECTORIO . 'css/temafinal.php';
-    if (file_exists($tm)) {
-      require_once $tm;
-    }
-  } ?>
 </head>
 
 <body>
@@ -85,19 +53,11 @@ require $AC_DIRECTORIO . 'datos/extenciones.php';
       <?= $AC_EXTRA == 'si' ? viewAdsMessageMovementAndBanner(CONFIG["ads"] ?? [], $AC_DIRECTORIO) : "" ?>
       <p class="titulo">
         <a href="<?php echo $AC_DIRECTORIO; ?>">Inicio</a> >
-        <?php echo $AC_TITULO;
-        if (isset($_GET['ac'])) {
-          echo ' > ' . $_GET['ac'];
-        }
-        ?>
+        <?= $AC_TITULO . (!empty($_GET["sc"]) ? ' > ' . secureString($_GET['sc']) : "") ?>
       </p>
-      <?php #CARGAR CONTENIDO DE LA PAGINA :3
-      if (isset($MENSAJE) && $MENSAJE == true) {
-        echo $lugarMensaje;
-      } ?>
-      <?php if (isset($AC_CONTENIDO)) {
-        echo $AC_CONTENIDO;
-      }
+      <?= !empty($MENSAJE) ? $lugarMensaje ?? "" : "" ?>
+      <?= $AC_CONTENIDO ?? "" ?>
+      <?php
       $elem = 2;
       $ex = 'scrDispladi';
       require $AC_DIRECTORIO . 'datos/extenciones.php';
