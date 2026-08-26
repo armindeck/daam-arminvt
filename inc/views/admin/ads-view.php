0465541b4185eead6772e4a46dde3fc4af02cee5
armindeck <dbhsoficial2003@gmail.com>
2026-08-25 23:32:42 -0500

    Refactor estructura del formulario en ads-view.php para mejorar la semántica y la presentación

--- inc/views/admin/ads-view.php
+++ inc/views/admin/ads-view.php
@@ -1,10 +1,10 @@
 <div class="flex flex-evenly">
-  <form method="post" style="width: 100%; max-width: 720px;">
-    <div class="formulario" style="width: 99%;">
-      <div class="p-4 t-strong">
-        Anuncios > Mensaje
-      </div>
-      <div class="flex flex-column gap-2">
+  <form method="post" style="width: 100%; max-width: 920px;">
+    <details class="formulario" style="width: 99%;" open>
+      <summary class="p-4 t-strong">
+        Anuncios: Mensaje
+      </summary>
+      <div class="flex flex-column gap-4">
         <textarea class="textarea-full" rows="5" name="message_content" placeholder="Mensaje"><?= CONFIG["ads"]["message"]["content"] ?? "" ?></textarea>
         <input type="url" name="message_link" placeholder="Enlace" value="<?= CONFIG["ads"]["message"]["link"] ?? "" ?>">
         <?= viewSelect("message_active", [["" => "No mostrar"], ["1" => "Mostrar"]], CONFIG["ads"]["message"]["active"] ?? "") ?>
@@ -27,13 +27,13 @@
       </div>
       <hr>
       <div class="flex flex-between p-8">
-        <button class="boton" type="reset">
+        <button class="boton2" type="reset">
           ❌ Cancelar
         </button>
         <button class="boton" type="submit" name="proccess" value="ads">
           💾 Guardar
         </button>
       </div>
-    </div>
+    </details>
   </form>
 </div>
