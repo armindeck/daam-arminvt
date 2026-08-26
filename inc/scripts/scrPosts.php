c43cd79891f4d102ba23d87f24e010359e04a309
armindeck <dbhsoficial2003@gmail.com>
2026-08-25 23:32:26 -0500

    Refactor lógica de generación de slug y acceso a directorios en scrPosts.php

--- inc/scripts/scrPosts.php
+++ inc/scripts/scrPosts.php
@@ -3,15 +3,15 @@
 if(isset($AC_DIRECTORIO)) return;
 if(!isset($id)) die("Posts no identificado");
 
-$slug = "/" . (ltrim(str_replace(".php", "", $id[1]), "/"));
+$slug = "/" . (ltrim(str_replace(".php", "", FILEPATH), "/"));
 $post_data = postSearchBySlug($slug, POSTS);
 
-$AC_DIRECTORIO = $id[0];
+$AC_DIRECTORIO = DIR;
 
 $acceso = true;
 
-$AC_UBICACION = !empty($id[1]) ? str_replace("./", "", rtrim(dirname($id[1]), "/") . "/") : "";
-$AC_ARCHIVO = basename($id[1]);
+$AC_UBICACION = !empty(FILEPATH) ? str_replace("./", "", rtrim(dirname(FILEPATH), "/") . "/") : "";
+$AC_ARCHIVO = basename(FILEPATH);
 $AC_METADESCRIPCION = $post_data["fragment"] ?? "";
 $AC_METADESCRIPCION2 = $post_data["fragment"] ?? "";
 $AC_METAETIQUETA = $post_data["tags"] ?? "";
@@ -23,4 +23,4 @@ $AC_DESCRIPCION = $post_data["fragment"] ?? "";
 $AC_FECHA = $post_data["date_last_updated"] ?? "";
 $AC_CONTENIDO = $post_data["content"] ?? "";
 $TIPO = !empty($post_data["type"]) ? ($post_data["type"] == "admin" ? "panel" : $post_data["type"]) : "";
-require_once $id[0].'datos/displa.php';
+require_once DIR.'datos/displa.php';
