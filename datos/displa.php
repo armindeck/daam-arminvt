5ca94db00cd5e6ed5eabae785d0d80375fd530da
armindeck <dbhsoficial2003@gmail.com>
2026-08-28 20:14:36 -0500

    Refactor displa.php: mejorar la estructura del HTML y actualizar rutas de recursos

--- datos/displa.php
+++ datos/displa.php
@@ -6,53 +6,47 @@ $URL_WEB = $_SERVER["HTTP_HOST"];
 $URL_FINAL = $_SERVER["REQUEST_URI"];
 $URL = $URL_WEB . $URL_FINAL;
 
-$ex = 'CargarTema';
-require $AC_DIRECTORIO . 'datos/extenciones.php';
-
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="es">
 
 <head>
-  <link rel="preload" href="<?php echo $AC_DIRECTORIO; ?>img/logo.png" as="image">
-  <link rel="icon" type="image/png" href="<?php echo $AC_DIRECTORIO; ?>img/logo.png" sizes="128x128">
-  <link rel="preload" href="<?= (CONFIG["page_name"] ?? "") . '/img/' . $AC_IMG; ?>" as="image">
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= (CONFIG["page_name"] ?? "") . ': ' . $AC_TITULO; ?></title>
-  <link rel="preload" href="<?php echo $AC_DIRECTORIO . 'css/' . $cargarEstilo; ?>" as="style" />
-  <link rel="stylesheet" type="text/css" href="<?php echo $AC_DIRECTORIO . 'css/' . $cargarEstilo; ?>">
-  <link rel="preload" href="<?php echo $AC_DIRECTORIO; ?>css/estilo.css" as="style" />
-  <link rel="stylesheet" type="text/css" href="<?php echo $AC_DIRECTORIO; ?>css/estilo.css">
-  <meta name="description" content="<?php echo $AC_METADESCRIPCION; ?>" />
-  <meta property="og:title" content="<?php echo $AC_TITULO; ?>" />
-  <meta property="og:description" content="<?php echo $AC_METADESCRIPCION2; ?>" />
-  <meta property="og:url" content="<?= (CONFIG["page_name"] ?? "").$URL_FINAL; ?>">
-  <link rel="canonical" href="<?= (CONFIG["page_name"] ?? "").$URL_FINAL; ?>">
+  <link rel="preload" href="<?= DIR ?>img/logo.png" as="image">
+  <link rel="icon" type="image/png" href="<?= DIR ?>img/logo.png" sizes="128x128">
+  <link rel="preload" href="<?= (CONFIG["page_name"] ?? "") . '/img/' . $AC_IMG; ?>" as="image">
+  <link rel="stylesheet" type="text/css" href="<?= DIR ?>css/estilo.css">
+  <meta name="description" content="<?= $AC_METADESCRIPCION ?? "" ?>" />
+  <meta property="og:title" content="<?= $AC_TITULO ?? "" ?>" />
+  <meta property="og:description" content="<?= $AC_METADESCRIPCION2 ?? "" ?>" />
+  <meta property="og:url" content="<?= (CONFIG["page_name"] ?? "") . $URL_FINAL; ?>">
+  <link rel="canonical" href="<?= (CONFIG["page_name"] ?? "") . $URL_FINAL; ?>">
   <meta property="og:image" content="<?= (CONFIG["page_link"] ?? "") . '/img/' . $AC_IMG; ?>" />
   <meta property="og:locale" content="es_CO" />
   <meta property="og:type" content="website">
   <meta property="og:site_name" content="<?= CONFIG["page_name"] ?? "" ?>" />
   <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:title" content="<?php echo $AC_TITULO; ?>">
-  <meta name="twitter:description" content="<?php echo $AC_METADESCRIPCION; ?>">
-  <meta name="twitter:image" content="<?php echo (CONFIG["page_link"] ?? "") . '/img/' . $AC_IMG; ?>">
+  <meta name="twitter:title" content="<?= $AC_TITULO ?? "" ?>">
+  <meta name="twitter:description" content="<?= $AC_METADESCRIPCION ?? "" ?>">
+  <meta name="twitter:image" content="<?= (CONFIG["page_link"] ?? "") . '/img/' . $AC_IMG; ?>">
   <meta name="keywords" content="<?= $AC_METAETIQUETA . ", " . (CONFIG["page_tags"] ?? "") ?>">
   <?= !empty(CONFIG["page_scripts_active"]) ? CONFIG["page_scripts"] ?? "" : "" ?>
 </head>
 
-<body>
+<body data-theme="<?= getTheme(CONFIG["theme"] ?? "") ?>">
   <?php $elem = 0;
   $ex = 'scrDispladi';
-  require $AC_DIRECTORIO . 'datos/extenciones.php';
+  require DIR . 'datos/extenciones.php';
   $elem = 1;
   $ex = 'scrDispladi';
-  require $AC_DIRECTORIO . 'datos/extenciones.php'; ?>
+  require DIR . 'datos/extenciones.php'; ?>
   <section>
     <main>
-      <?= $AC_EXTRA == 'si' ? viewAdsMessageMovementAndBanner(CONFIG["ads"] ?? [], $AC_DIRECTORIO) : "" ?>
+      <?= $AC_EXTRA == 'si' ? viewAdsMessageMovementAndBanner(CONFIG["ads"] ?? [], DIR) : "" ?>
       <p class="titulo">
-        <a href="<?php echo $AC_DIRECTORIO; ?>">Inicio</a> >
+        <a href="<?= DIR ?>">Inicio</a> >
         <?= $AC_TITULO . (!empty($_GET["sc"]) ? ' > ' . secureString($_GET['sc']) : "") ?>
       </p>
       <?= !empty($MENSAJE) ? $lugarMensaje ?? "" : "" ?>
@@ -60,22 +54,22 @@ require $AC_DIRECTORIO . 'datos/extenciones.php';
       <?php
       $elem = 2;
       $ex = 'scrDispladi';
-      require $AC_DIRECTORIO . 'datos/extenciones.php';
+      require DIR . 'datos/extenciones.php';
       if (isset($forimg)) {
         echo '<div class="flexCon">';
         for ($i = 0; $i <= 25; $i++) {
           echo '<div class="m2">
-                    <div class="imagen"><a target="_blank" href="' . $AC_DIRECTORIO . 'img/chb' . $i . '.PNG"><img class="img1" loading="lazy" src="' . $AC_DIRECTORIO . 'img/chb' . $i . '.PNG" alt="' . $AC_TITULO . '" title="' . $AC_TITULO . '"></a></div>
+                    <div class="imagen"><a target="_blank" href="' . DIR . 'img/chb' . $i . '.PNG"><img class="img1" loading="lazy" src="' . DIR . 'img/chb' . $i . '.PNG" alt="' . $AC_TITULO . '" title="' . $AC_TITULO . '"></a></div>
                 </div>';
         }
         echo '</div>';
       }
       if (!isset($ERROR_DARFORMATO)) {
         $ex = 'DarFormato';
-        require $AC_DIRECTORIO . 'datos/extenciones.php';
+        require DIR . 'datos/extenciones.php';
       }
       if (isset($TIPO)) {
-        $iobi_dir = $AC_DIRECTORIO . 'form/iobi/';
+        $iobi_dir = DIR . 'form/iobi/';
         if ($TIPO == 'blog') {
           if (isset($_SESSION['rol']) && $_SESSION['rol'] == 5) {
             $accesoIobiform = true;
@@ -90,18 +84,18 @@ require $AC_DIRECTORIO . 'datos/extenciones.php';
           case 'panel':
             $NoAumentarContador = true;
             echo $lugarMensaje;
-            require $AC_DIRECTORIO . 'inc/views/admin-view.php';
+            require DIR . 'inc/views/admin-view.php';
             break;
           case 'entradas':
-            $entradas = readJson(pathData()."/entries.json");
+            $entradas = readJson(pathData() . "/entries.json");
             $ex = 'CargarEntradas';
-            require $AC_DIRECTORIO . 'datos/extenciones.php';
+            require DIR . 'datos/extenciones.php';
             break;
         }
       }
       if (isset($GALERIA) && $GALERIA == true) {
         $ex = 'CargarImagenes';
-        require $AC_DIRECTORIO . 'datos/extenciones.php';
+        require DIR . 'datos/extenciones.php';
       }
       if (isset($accesoIobiform) && $accesoIobiform == true) {
         if (file_exists($iobi_dir . 'formulario.php')) {
@@ -115,23 +109,15 @@ require $AC_DIRECTORIO . 'datos/extenciones.php';
           require $iobi_dir . 'cargar.php';
         }
       }
-      
-      if (isset($TIPO) && $TIPO != 'panel' or !isset($TIPO)): ?>
-        <div class="titulo t18">Compartir en
-          <a href="https://facebook.com/sharer.php?u=<?php echo $URL; ?>" title="Compartir en Facebook" rel="nofollow" target="_blank" href=""><i class="fab fa-facebook"></i></a>
-          <a href="https://twitter.com/share?url=<?php echo $URL; ?>" title="Compartir en Twitter" rel="nofollow" target="_blank" href=""><i class="fab fa-twitter"></i></a>
-          <a href="https://t.me/share/url?url=<?php echo $URL; ?>" title="Compartir en Telegram" rel="nofollow" target="_blank" href=""><i class="fab fa-telegram"></i></a>
-          <a href="https://api.whatsapp.com/send?text=<?php echo $URL; ?>" title="Compartir en WhatsApp" rel="nofollow" target="_blank" href=""><i class="fab fa-whatsapp"></i></a>
-        </div>
-      <?php endif; ?>
+      ?>
     </main>
     <?php $elem = 3;
     $ex = 'scrDispladi';
-    require $AC_DIRECTORIO . 'datos/extenciones.php'; ?>
+    require DIR . 'datos/extenciones.php'; ?>
   </section>
   <?php $elem = 4;
   $ex = 'scrDispladi';
-  require $AC_DIRECTORIO . 'datos/extenciones.php'; ?>
+  require DIR . 'datos/extenciones.php'; ?>
 </body>
 
 </html>
