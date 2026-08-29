6ddc5c52a96d022b49b9adebc63cde322847f200
armindeck <dbhsoficial2003@gmail.com>
2026-08-29 13:40:22 -0500

    Actualizar la asignación de variables en scrPosts.php para utilizar datos de POST en lugar de postSearchBySlug

--- inc/scripts/scrPosts.php
+++ inc/scripts/scrPosts.php
@@ -3,23 +3,21 @@
 if(isset($AC_DIRECTORIO)) return;
 if(!isset($id)) die("Posts no identificado");
 
-$post_data = postSearchBySlug(SLUG, POSTS);
-
 $AC_DIRECTORIO = DIR;
 
 $acceso = true;
 
 $AC_UBICACION = !empty(FILEPATH) ? str_replace("./", "", rtrim(dirname(FILEPATH), "/") . "/") : "";
 $AC_ARCHIVO = basename(FILEPATH);
-$AC_METADESCRIPCION = $post_data["fragment"] ?? "";
-$AC_METADESCRIPCION2 = $post_data["fragment"] ?? "";
-$AC_METAETIQUETA = $post_data["tags"] ?? "";
-$AC_IMG = $post_data["image"] ?? "";
-$AC_EXTRA = $post_data["tags"] ?? "";
-$AC_TITULO = $post_data["title"] ?? "";
-$AC_CATALOGO = $post_data["catalog"] ?? "";
-$AC_DESCRIPCION = $post_data["fragment"] ?? "";
-$AC_FECHA = $post_data["date_last_updated"] ?? "";
-$AC_CONTENIDO = $post_data["content"] ?? "";
-$TIPO = !empty($post_data["type"]) ? ($post_data["type"] == "admin" ? "panel" : $post_data["type"]) : "";
+$AC_METADESCRIPCION = POST["fragment"] ?? "";
+$AC_METADESCRIPCION2 = POST["fragment"] ?? "";
+$AC_METAETIQUETA = POST["tags"] ?? "";
+$AC_IMG = POST["image"] ?? "";
+$AC_EXTRA = POST["tags"] ?? "";
+$AC_TITULO = POST["title"] ?? "";
+$AC_CATALOGO = POST["catalog"] ?? "";
+$AC_DESCRIPCION = POST["fragment"] ?? "";
+$AC_FECHA = POST["date_last_updated"] ?? "";
+$AC_CONTENIDO = POST["content"] ?? "";
+$TIPO = POST["type"] ?? "";
 require_once DIR.'datos/displa.php';
