fdb702faa56e7e6a1b4070b2a664199712b70ed7
armindeck <dbhsoficial2003@gmail.com>
2026-08-29 12:15:21 -0500

    Actualizar layout-view.php para mejorar la gestión de metadatos y estilos de página.

--- inc/views/layout-view.php
+++ inc/views/layout-view.php
@@ -1,38 +1,29 @@
 <!-- daam core v<?= CORE["core_version"] . "-" . CORE["core_state"] ?> (Copyright © 2023 Armin Deck – Licencia de Uso No Transferible) – https://github.com/armindeck/daam-arminvt -->
-<?php
-
-$DIRECTORIO_AQUI = $_SERVER["REQUEST_URI"];
-$URL_WEB = $_SERVER["HTTP_HOST"];
-$URL_FINAL = $_SERVER["REQUEST_URI"];
-$URL = $URL_WEB . $URL_FINAL;
-
-?>
 <!DOCTYPE html>
 <html lang="<?= CONFIG["page_language"] ?? "es" ?>">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= (CONFIG["page_name"] ?? "") . ': ' . $AC_TITULO; ?></title>
+  <title><?= ($AC_TITULO ?? "") . " - " . (CONFIG["page_name"] ?? "") ?></title>
   <link rel="preload" href="<?= DIR ?>img/logo.png" as="image">
   <link rel="icon" type="image/png" href="<?= DIR ?>img/logo.png" sizes="128x128">
-  <link rel="preload" href="<?= (CONFIG["page_name"] ?? "") . '/img/' . $AC_IMG; ?>" as="image">
-  <link rel="stylesheet" type="text/css" href="<?= DIR ?>css/estilo.css">
   <meta name="description" content="<?= $AC_METADESCRIPCION ?? "" ?>" />
   <meta property="og:title" content="<?= $AC_TITULO ?? "" ?>" />
   <meta property="og:description" content="<?= $AC_METADESCRIPCION2 ?? "" ?>" />
-  <meta property="og:url" content="<?= (CONFIG["page_name"] ?? "") . $URL_FINAL; ?>">
-  <link rel="canonical" href="<?= (CONFIG["page_name"] ?? "") . $URL_FINAL; ?>">
-  <meta property="og:image" content="<?= (CONFIG["page_link"] ?? "") . '/img/' . $AC_IMG; ?>" />
-  <meta property="og:locale" content="es_CO" />
-  <meta property="og:type" content="website">
+  <meta property="og:url" content="<?= URL_NOT_INDEX ?>">
+  <link rel="canonical" href="<?= URL_NOT_INDEX ?>">
+  <meta property="og:image" content="<?= DIR . '/img/' . $AC_IMG; ?>" />
   <meta property="og:site_name" content="<?= CONFIG["page_name"] ?? "" ?>" />
   <meta name="twitter:card" content="summary_large_image">
   <meta name="twitter:title" content="<?= $AC_TITULO ?? "" ?>">
   <meta name="twitter:description" content="<?= $AC_METADESCRIPCION ?? "" ?>">
-  <meta name="twitter:image" content="<?= (CONFIG["page_link"] ?? "") . '/img/' . $AC_IMG; ?>">
-  <meta name="keywords" content="<?= $AC_METAETIQUETA . ", " . (CONFIG["page_tags"] ?? "") ?>">
+  <meta name="twitter:image" content="<?= DIR . '/img/' . $AC_IMG; ?>">
+  <meta name="keywords" content="<?= ($AC_METAETIQUETA ?? "") . ", " . (CONFIG["page_tags"] ?? "") ?>">
   <?= !empty(CONFIG["page_scripts_active"]) ? CONFIG["page_scripts"] ?? "" : "" ?>
+  <style type="text/css">
+    <?= file_exists(DIR."css/".(CONFIG["page_style"] ?? "")) ? file_get_contents(DIR."css/".(CONFIG["page_style"] ?? "")) ?? "" : "" ?>
+  </style>
 </head>
 
 <body data-theme="<?= getTheme(CONFIG["page_theme"] ?? "") ?>">
