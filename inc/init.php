74673668627f332f841776a0b3dfe696131e4920
armindeck <dbhsoficial2003@gmail.com>
2026-09-12 16:29:31 -0500

    Mejorar la seguridad de la sesión y agregar archivo de control de acceso

--- inc/init.php
+++ inc/init.php
@@ -1,5 +1,12 @@
 <?php
 
+session_start([
+  "cookie_secure" => true, // Solo HTTPS
+  "cookie_httponly" => true, // No accesible desde JS
+  "cookie_samesite" => "lax", // Protección CSRF
+  "use_strict_mode" => true // Evita session fixation
+]);
+
 require_once __DIR__ . "/function.php";
 require_once __DIR__ . "/function-deprecated.php";
 require_once __DIR__ . "/lib/Markdown.php";
@@ -8,7 +15,7 @@ require_once __DIR__ . "/lib/MarkdownExtra.php";
 generateFilesData();
 
 define("DIR", $AC_DIRECTORIO ?? $id[0] ?? "./");
-define("FILEPATH", !empty(($AC_UBICACION ?? "") . ($AC_ARCHIVO ?? "")) ? $AC_UBICACION.$AC_ARCHIVO : $id[1] ?? "");
+define("FILEPATH", !empty(($AC_UBICACION ?? "") . ($AC_ARCHIVO ?? "")) ? $AC_UBICACION . $AC_ARCHIVO : $id[1] ?? "");
 define("SLUG", "/" . (ltrim(str_replace(".php", "", FILEPATH), "/")));
 define("CORE", readJson(pathDataCore()));
 define("CONFIG", readJson(pathDataConfig()));
@@ -23,42 +30,23 @@ define("PHP_EXTENSION", (CONFIG["page_extension_php_active"] ?? false) ? ".php" 
 define("URL", (CONFIG["page_link"] ?? "") . SLUG);
 define("URL_NOT_INDEX", (CONFIG["page_link"] ?? "") . rtrim(SLUG, "index"));
 
-session_start([
-  "cookie_secure" => true, // Solo HTTPS
-  "cookie_httponly" => true, // No accesible desde JS
-  "cookie_samesite" => "lax", // Protección CSRF
-  "use_strict_mode" => true // Evita session fixation
-]);
-
 date_default_timezone_set(CONFIG["page_timezone"] ?? "America/Bogota");
 error_reporting(CONFIG["page_debug_active"] ?? false);
 
 loginAdminDeprecated();
-
+//register("admin", "Admin", "admin@example.com", "admin123", "admin123", USERS);
+//var_dump(login("admin", "admin123", USERS));
+//var_dump(auth());
 # AUTH VERIFY
-if (auth() && !authVerify(USERS)) {
-  if (logout()) redirect(DIR."iniciar.php");
-  redirect(DIR."error");
-}
-
-if (!empty($_GET["logout"])) {
-  if (logout()) redirect(DIR."iniciar.php?ms=exi&msm=sesionfinalizada");
-  redirect(DIR."error");
-}
-
 setTheme();
-
-if (SLUG == "/admin"){
-  //if(!auth() || !isAdmin()) redirect(DIR."iniciar.php?ms=err&msm=accdenegado");
-  require_once __DIR__."/admin.php";
-}
-
 setVisits(SLUG, VISITS);
 
-require_once __DIR__."/scripts/scrPosts.php";
+require_once __DIR__ . "/web.php";
 
-$adminprivado = readJson(pathData()."/admin-private-deprecated.json");
+require_once __DIR__ . "/scripts/scrPosts.php";
 
-require_once DIR.'datos/mensajes.php';
-$ex='CargarTema';
-require_once DIR.'datos/extenciones.php';
+$adminprivado = readJson(pathData() . "/admin-private-deprecated.json");
+
+require_once DIR . 'datos/mensajes.php';
+$ex = 'CargarTema';
+require_once DIR . 'datos/extenciones.php';
---
+++ inc/web.php
@@ -0,0 +1,24 @@
+<?php
+
+// Redirect to error page if user is not authenticated or if user is authenticated but not verified, and trying to access /profile or /admin
+if (auth() && !authVerify(USERS) || in_array(SLUG, ["/profile", "/admin"]) && !auth()) {
+    if (logout()) redirect(DIR . "login" . PHP_EXTENSION);
+    redirect(DIR . "error");
+}
+
+if (!empty($_GET["logout"])) {
+    if (auth()) {
+        if (logout()) redirect(DIR . "login" . PHP_EXTENSION . "?ms=exi&msm=sesionfinalizada");
+        redirect(DIR . "error");
+    }
+}
+
+if (in_array(SLUG, ["/login", "/register", "/forgot-password"])) {
+    if (auth()) redirect(DIR . "profile" . PHP_EXTENSION);
+    require_once __DIR__ . "/auth.php";
+}
+
+if (SLUG == "/admin") {
+    if (!isAdmin()) redirect(DIR . "login" . PHP_EXTENSION . "?ms=err&msm=accdenegado");
+    require_once __DIR__ . "/admin.php";
+}
