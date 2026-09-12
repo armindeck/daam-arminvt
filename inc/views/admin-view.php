9f279be0f80dd1daa381fca4381ebe09e9c45bae
armindeck <dbhsoficial2003@gmail.com>
2026-09-12 16:30:47 -0500

    Agregar clase CSS al elemento de navegación en la vista de administrador

--- inc/views/admin-view.php
+++ inc/views/admin-view.php
@@ -2,7 +2,7 @@
 
 $sections = implode("", array_map(fn($section) => "<a href=\"?sc={$section['id']}\"><i class=\"{$section['icon']}\"></i> {$section['label']}</a>", ADMIN["section"] ?? ""));
 
-echo "<nav>$sections</nav>";
+echo "<nav class=\"nav\">$sections</nav>";
 
 echo $show_message_admin ? "<div class=\"p-8 t-center\">No existe la sección <strong>$get_sc</strong> o esta en construcción.</div>" : "";
 
