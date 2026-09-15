ef63763a8bb0303717bf1a121c7709ef054d68f7
armindeck <dbhsoficial2003@gmail.com>
2026-09-14 20:28:03 -0500

    Update auth-nav-view layout for improved spacing between links

--- inc/views/components/auth-nav-view.php
+++ inc/views/components/auth-nav-view.php
@@ -1,8 +1,8 @@
 <h1 class="t-center"><?= $title ?></h1>
 <hr>
-<header class="flex flex-center items-center">
-    <a class="boton boton2" href="<?= DIR . "login" . PHP_EXTENSION ?>">Login</a> |
-    <a class="boton boton2" href="<?= DIR . "register" . PHP_EXTENSION ?>">Register</a> |
+<header class="flex flex-center items-center gap-8">
+    <a class="boton boton2" href="<?= DIR . "login" . PHP_EXTENSION ?>">Login</a>
+    <a class="boton boton2" href="<?= DIR . "register" . PHP_EXTENSION ?>">Register</a>
     <a class="boton boton2" href="<?= DIR . "forgot-password" . PHP_EXTENSION ?>">Forgot password</a>
 </header>
 <hr>
