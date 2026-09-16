79b50f739e932070ce37d8787156e1e6e3d1b376
armindeck <dbhsoficial2003@gmail.com>
2026-09-15 23:46:56 -0500

    Reestructurar la vista de subida de imágenes: simplificar el código y mejorar la interfaz de usuario

--- inc/views/admin/upload-image-view.php
+++ inc/views/admin/upload-image-view.php
@@ -1,29 +1,13 @@
-<?php if(isset($TIPO) && $TIPO=='panel'){ ?>
-
-<p class="texini">Subida de imagenes</p>
-<form class="formulario" method="post" action="imagen/subir.php" enctype="multipart/form-data">
-	<label>Antes de subir una imagen usa: <a target="_blank" href="https://tinypng.com/" rel="nofollow">TinyPNG</a>, para optimizar la imagen.</label><hr>
-	<span>Seleccionar imagenes</span><br>
-	<input type="file" accept=".jpg,.jpeg,.png,.gif" name="imagen" required><hr>
-	<label>Agrega un nombre -> opcional</label><br>
-	<input type="text" name="imagen_nombre" placeholder="nombre.png"><hr>
-	<input name="IniSubirImagen" type="submit" value="Subir &#xf0ee"><hr>
-	<span class="t14"><?php echo file_get_contents(__DIR__.'/imagen.x'); ?></span>
-</form>
-<?php
-
-if (isset($_GET['dir'])): ?>
-
-<hr><a class="boton" target="_blank" href="<?php echo $_GET['dir']; ?>">Mostrar <i class="fas fa-external-link-alt"></i></a>
-
-<?php endif; ?>
-
-
-
-<?php } else {
-
-    if(isset($AC_DIRECTORIO)){ $AC_DIRECTORIO=$AC_DIRECTORIO; } else { $AC_DIRECTORIO='../../'; }
-
-    $vamos=$AC_DIRECTORIO."error.php?ms=err&msm=accdenegado"; header("Location: {$vamos}");
-
-} ?>
+<main class="flex flex-evenly flex-1">
+	<form method="post" class="form-3" enctype="multipart/form-data">
+		<div class="flex flex-column gap-6">
+			<h2 class="p-8 t-center">Subir imagen</h2>
+			<p>Antes de subir una imagen usa: <a target="_blank" href="https://tinypng.com/" rel="nofollow">TinyPNG</a>, para optimizar la imagen.</p>
+			<input type="file" accept=".jpg,.jpeg,.png,.gif" name="image" required>
+			<input type="text" name="image_name" placeholder="name.png (opcional)">
+			<button class="boton" type="submit" name="proccess" value="upload-image">
+				<i class="fas fa-upload"></i> Subir imagen
+			</button>
+		</div>
+	</form>
+</main>
