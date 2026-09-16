c924e96e20b0d9541a34ee95555e7588abbff32e
armindeck <dbhsoficial2003@gmail.com>
2026-09-15 23:34:52 -0500

    Refactor variables in admin.php to use the $admin array for better organization and clarity

--- inc/admin.php
+++ inc/admin.php
@@ -1,14 +1,19 @@
 <?php
 
-$get_sc = secureString($_GET['sc'] ?? "");
-$sc_in_section = in_array($get_sc, array_map(fn($section) => $section["id"], ADMIN["section"] ?? ""));
-$path_file_section_admin = __DIR__."/views/admin/{$get_sc}-view.php";
-$path_file_section_admin_process = __DIR__."/actions/admin/{$get_sc}.php";
-$file_exists_section_admin = file_exists($path_file_section_admin);
-$file_exists_section_admin_process = file_exists($path_file_section_admin_process);
-$show_message_admin = !empty($get_sc) && (!$file_exists_section_admin || !$sc_in_section);
-$load_section_admin = $sc_in_section && $file_exists_section_admin;
+$admin["get_sc"] = secureString($_GET['sc'] ?? "");
+$admin["sc_in_section"] = in_array($admin['get_sc'], array_map(fn($section) => $section["id"], ADMIN["section"] ?? ""));
+$admin["path_file_section_admin"] = __DIR__ . "/views/admin/{$admin['get_sc']}-view.php";
+$admin["path_file_section_admin_process"] = __DIR__ . "/actions/admin/{$admin['get_sc']}.php";
+$admin["file_exists_section_admin"] = file_exists($admin["path_file_section_admin"]);
+$admin["file_exists_section_admin_process"] = file_exists($admin["path_file_section_admin_process"]);
+$admin["show_message_admin"] = !empty($admin['get_sc']) && (!$admin["file_exists_section_admin"] || !$admin["sc_in_section"]);
+$admin["load_section_admin"] = $admin["sc_in_section"] && $admin["file_exists_section_admin"];
+$admin["alert"] = [
+    "active" => getAlert()["active"] ?? $admin["show_message_admin"] ?? false,
+    "type" => getAlert()["type"] ?? ($admin["show_message_admin"] ? "error" : ""),
+    "message" => getAlert()["message"] ?? ($admin["show_message_admin"] ? "No existe la sección <strong>{$admin['get_sc']}</strong> o esta en construcción." : "")
+];
 
-if($load_section_admin && $file_exists_section_admin_process){
-    require_once $path_file_section_admin_process;
+if ($admin["load_section_admin"] && $admin["file_exists_section_admin_process"]) {
+    require_once $admin["path_file_section_admin_process"];
 }
